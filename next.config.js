4a84e17c13dd967a79bbdfde16073f42a00b6ffe
Travis Fischer <fisch0920@gmail.com>
2021-01-18 22:25:28 -0500

    🥓

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-  images: {
-    domains: ['ssfy.io']
-  }
+  // images: {
+  //   domains: ['ssfy.io']
+  // }
 })
