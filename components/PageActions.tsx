15b289463e8cc399c0d07b4b346d9c8e6194f386
Travis Fischer <fisch0920@gmail.com>
2021-01-25 17:51:17 -0500

    😱

--- components/PageActions.tsx
+++ components/PageActions.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 import * as types from 'lib/types'
 
-import styles from './styles.module.css'
+// import styles from './styles.module.css'
 
 export const PageHead: React.FC<types.PageProps> = ({ site }) => {
-  return (
- 
-  )
+  return null
 }
