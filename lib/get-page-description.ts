f35d8eb0eb1a3b5d71f387bef9b37fa11bc64442
Travis Fischer <fisch0920@gmail.com>
2021-01-25 16:30:49 -0500

    styling updates

--- lib/get-page-description.ts
+++ lib/get-page-description.ts
@@ -1,21 +1,9 @@
-import * as types from 'lib/types'
-import { getTextContent } from 'notion-utils'
+import * as types from './types'
+import { getPageProperty } from 'notion-utils'
 
 export function getPageDescription(
   block: types.Block,
-  recordMap: types.RecordMap
+  recordMap: types.ExtendedRecordMap
 ): string | null {
-  const collection = recordMap.collection[block.parent_id]?.value
-
-  if (collection) {
-    const descriptionKey = Object.keys(collection.schema).find(
-      (key) => collection.schema[key].name === 'Description'
-    )
-
-    if (descriptionKey) {
-      return getTextContent(block.properties[descriptionKey])
-    }
-  }
-
-  return null
+  return getPageProperty('Description', block, recordMap)
 }
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@
 
 # Next.js Notion Starter Kit
 
-> The perfect starter kit for building beautiful websites with Next.js and Notion.
+> The perfect starter kit for building websites with Next.js and Notion.
 
 [![Build Status](https://travis-ci.com/transitive-bullshit/nextjs-notion-starter-kit.svg?branch=main)](https://travis-ci.com/transitive-bullshit/nextjs-notion-starter-kit) [![Prettier Code Formatting](https://img.shields.io/badge/code_style-prettier-brightgreen.svg)](https://prettier.io)
 
