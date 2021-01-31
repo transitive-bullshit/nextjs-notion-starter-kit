88e7fdc438596c2f582f85291a245dc4b2b05e0d
Travis Fischer <fisch0920@gmail.com>
2021-01-30 20:30:46 -0500

    feat: update next.js and minor style fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,8 @@ import 'styles/notion.css'
 // global style overrides for prism theme
 import 'styles/prism-theme.css'
 
+// here we're bringing in any languages we want to support for
+// syntax highlighting via Notion's Code block
 import 'prismjs'
 import 'prismjs/components/prism-markup'
 import 'prismjs/components/prism-javascript'
