09146897734f9d5b16be6243eaa6204eaf7d74d4
Travis Fischer <fisch0920@gmail.com>
2021-02-07 21:37:21 -0500

    feat: adjust hero animation

--- components/HeroHeader.tsx
+++ components/HeroHeader.tsx
@@ -5,6 +5,8 @@ import random from 'random'
 
 import FluidAnimation from 'react-fluid-animation'
 
+const exp = random.exponential()
+const numSplatsPerEpoch = 1
 const minSplatRadius = 0.01
 const maxSplatRadius = 0.03
 
@@ -14,6 +16,7 @@ export class HeroHeader extends Component<{
   _time: number
   _direction: number
   _tickRaf: any
+  _timeout: any
   _animation: any
 
   componentDidMount() {
@@ -30,6 +33,11 @@ export class HeroHeader extends Component<{
     if (this._tickRaf) {
       raf.cancel(this._tickRaf)
       this._tickRaf = null
+    }
+
+    if (this._timeout) {
+      clearTimeout(this._timeout)
+      this._timeout = null
     }
   }
 
@@ -58,18 +66,27 @@ export class HeroHeader extends Component<{
   }
 
   _tick = () => {
-    // TODO: make this more efficient
-    if (this._animation && random.float() < 0.15) {
+    this._tickRaf = null
+    this._timeout = null
+
+    let scale = 1.0
+
+    if (this._animation) {
       const w = this._animation.width
       const h = this._animation.height
 
+      // adjust the intensity scale depending on the canvas width, so it's less
+      // intense on smaller screens
+      const s = Math.max(0.1, Math.min(1, w / 1200))
+      scale = Math.pow(s, 1.2)
+
       this._animation.config.splatRadius = random.float(
-        minSplatRadius,
-        maxSplatRadius
+        minSplatRadius * scale,
+        maxSplatRadius * scale
       )
 
       const splats = []
-      for (let i = 0; i < 1; ++i) {
+      for (let i = 0; i < numSplatsPerEpoch; ++i) {
         const color = [random.float(10), random.float(10), random.float(10)]
 
         const w0 = w / 3.0
@@ -81,17 +98,20 @@ export class HeroHeader extends Component<{
         while (true) {
           const x = random.float(w)
           const y = random.float(h)
+
+          // favor uniformly distributed samples within the center-ish of the canvas
           if (x > w0 && x < w1 && y > h0 && y < h1) {
             continue
           }
 
-          const dx = random.float(-1, 1) * random.float(200, 3000)
-          const dy = random.float(-1, 1) * random.float(200, 3000)
+          const dx = random.float(-1, 1) * random.float(200, 3000) * scale
+          const dy = random.float(-1, 1) * random.float(200, 3000) * scale
           const splat = { x, y, dx, dy, color }
           splats.push(splat)
           break
         }
 
+        // old version which generated samples along a circle
         // const t = random.float(2 * Math.PI)
         // const cos = Math.cos(t)
         // const sin = Math.sin(t)
@@ -107,6 +127,13 @@ export class HeroHeader extends Component<{
       this._animation.addSplats(splats)
     }
 
-    this._tickRaf = raf(this._tick)
+    // using an exponential distribution here allows us to favor bursts of activity
+    // but also allow for more occasional pauses
+    const dampenedScale = Math.pow(scale, 0.2)
+    const timeout = (exp() * 100) / dampenedScale
+
+    this._timeout = setTimeout(() => {
+      this._tickRaf = raf(this._tick)
+    }, timeout)
   }
 }
