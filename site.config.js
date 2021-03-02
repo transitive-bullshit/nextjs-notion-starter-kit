6a49dfbe805866ebcd51fe7bb256ad018967386b
reddy2go <reddy2go@hey.com>
2021-03-02 22:24:37 +0530

    Customised to Reddy2go

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '623e68bd14a442c483e7710b25d39f81',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jungle Book',
+  domain: 'junglebook.reddy2go.com',
+  author: 'Reddy2Go',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'a Digital Forest of unfinished & overgrown thoughts',
+  socialImageTitle: 'The Reddy2Go Jungle Book',
+  socialImageSubtitle: 'Lose yourself <> Find yourself',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'reddy2go',
+  github: '',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
