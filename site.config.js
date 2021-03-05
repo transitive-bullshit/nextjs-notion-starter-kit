3016cbaa032afda37bf25d626b329137a4113fc0
アレクサンダー.eth <git@.com>
2021-03-04 19:10:19 -0500

    update

--- site.config.js
+++ site.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
   rootNotionPageId: 'ff1a3cae900941e49cc4d4458cc2867d',
-
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: '684fb1cf-ad2b-40c7-8e31-c099fa61e45f',
