f6a4e479ef1385c02d58c4dca17ea1fa9df909ee
lizhimiao <untildied@gmail.com>
2021-04-06 14:29:29 +0800

    Update site.config.js
    修改评论代码

--- site.config.js
+++ site.config.js
@@ -32,7 +32,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: "https://github.com/zhimiaoli/lizhimiao-dot-com-comment",
+  utterancesGitHubRepo: "zhimiaoli/lizhimiao-dot-com-comment",
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
