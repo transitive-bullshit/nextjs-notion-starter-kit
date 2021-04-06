69cf179c27ed1689ca60f3f21174c407430eb022
lizhimiao <untildied@gmail.com>
2021-04-06 13:29:01 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '9fc4005ace274416bf5d756d02c4700a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'lizhimiao的博客',
+  domain: 'blog.lizhimiao.com',
+  author: 'zhimiao li',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -18,8 +18,8 @@ module.exports = {
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  github: 'zhimiaoli',
+  linkedin: 'lizhimiao',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
