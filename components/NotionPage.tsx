d0461ddd5227fc7b5126210b9d8c975212407415
Travis Fischer <fisch0920@gmail.com>
2021-04-06 23:20:47 -0400

    feat: update deps

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -9,7 +9,7 @@ import BodyClassName from 'react-body-classname'
 import useDarkMode from 'use-dark-mode'
 import { PageBlock } from 'notion-types'
 
-import { Tweet, Twitter } from 'react-static-tweets'
+import { Tweet, TwitterContextProvider } from 'react-static-tweets'
 
 // core notion renderer
 import { NotionRenderer, Code, Collection, CollectionRow } from 'react-notion-x'
@@ -161,7 +161,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
   }
 
   return (
-    <Twitter.Provider
+    <TwitterContextProvider
       value={{
         tweetAstMap: (recordMap as any).tweetAstMap || {},
         swrOptions: {
@@ -281,6 +281,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
       <GitHubShareButton />
 
       <CustomHtml site={site} />
-    </Twitter.Provider>
+    </TwitterContextProvider>
   )
 }
