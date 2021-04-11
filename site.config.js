4155ef1a9de398fe418401db9811f0e019975455
wz1616 <62297116+wz1616@users.noreply.github.com>
2021-04-11 18:12:57 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: 'd6e79d6e0bd843d1bbdb220868ed92a9',
+  rootNotionPageId: '3e492e44ec994973b593fa5efe227df2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '翼轸',
-  domain: 'https://www.notion.so/wz16/d6e79d6e0bd843d1bbdb220868ed92a9',
+  name: '翼轸的小站',
+  domain: 'https://www.notion.so/wz16/3e492e44ec994973b593fa5efe227df2?v=27bf665c9c1a4c43bb81befebf31a0a0',
   author: '翼轸',
 
   // open graph metadata (optional)
-  description: 'Example site description',
+  description: 'Yizhen site',
   socialImageTitle: 'Transitive Bullshit',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: '',
-  github: 'wz1616',
-  linkedin: '',
+  twitter: null,
+  github: null,
+  linkedin: null,
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
