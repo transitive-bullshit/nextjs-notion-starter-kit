4504df47023fe0bb9e294151cbe8ba79ad551162
garthallan-developer <67453954+garthallan-developer@users.noreply.github.com>
2021-04-12 15:11:42 +0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,17 +1,14 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
-  rootNotionPageId: 'bdb856d476f740cda6987a9e60999bca',
+  rootNotionPageId: '2ff3e553b24245b4b7a86cea4dc36306',
+
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
-  name: 'notion.garthallan',
+  name: 'Garth Allan',
   domain: 'notion.garthallan.com',
   author: 'Garth Allan,
   // open graph metadata (optional)
@@ -20,10 +17,6 @@ module.exports = {
   socialImageTitle: 'Garth Allan',
   socialImageSubtitle: 'Hello World! 👋',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
   // social usernames (optsional)
   twitter: 'garthallan',
   github: 'garthallan-developer',
