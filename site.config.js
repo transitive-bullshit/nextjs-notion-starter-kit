a693f3a85f3e610526b7e9562cccbfb477979926
garthallan-developer <67453954+garthallan-developer@users.noreply.github.com>
2021-04-12 15:12:38 +0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -22,5 +22,31 @@ module.exports = {
   github: 'garthallan-developer',
   linkedin: 'garthallan',
 
-  // default notion icon and cover images for site-wide consistency (optional)
+   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+  utterancesGitHubRepo: null,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
