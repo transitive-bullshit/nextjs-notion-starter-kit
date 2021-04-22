eab59b2efef811ed05a7581df934e980edcef4a9
アレクサンダー.eth <git@.com>
2021-04-21 23:50:50 -0500

    breadcrumb glow up

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,14 +26,14 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+      <div className={styles.copyright}>&copy; 2021 {config.author}</div>
 
       {hasMounted ? (
         <div className={styles.settings}>
           <a
             className={styles.toggleDarkMode}
             onClick={toggleDarkModeCb}
-            title='Tottle dark mode'
+            title='Throttle dark mode'
           >
             {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
           </a>
