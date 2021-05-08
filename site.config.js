a46bc2bdaeb5cbb95026513e99b10d917853c405
co <51730273+c0ralinee@users.noreply.github.com>
2021-05-08 11:50:36 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'a70321a892c24d9ca05f0c51b80b9934',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Co Box',
+  domain: 'https://www.notion.so/Co-s-Blog-a70321a892c24d9ca05f0c51b80b9934',
+  author: 'Coraline',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  Bilibili: '是co鸭',
+  NetEaseMusic: 'coooraline',
+  Ins: 'coral1nee',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
