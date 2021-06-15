ee0366995966085a3d401f6c1705248e090df418
Travis Fischer <fisch0920@gmail.com>
2021-06-15 03:30:45 -0400

    👽

--- site.config.js
+++ site.config.js
@@ -39,7 +39,7 @@ module.exports = {
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
   // variables specified in .env.example
-  isPreviewImageSupportEnabled: true,
+  isPreviewImageSupportEnabled: false,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
