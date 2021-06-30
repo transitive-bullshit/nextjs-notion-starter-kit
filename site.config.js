8a90b67ded23e72a3919fa9b6ee5de0532d3dabe
ilongmao <31371881+ilongmao@users.noreply.github.com>
2021-06-30 15:00:37 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: 'goldyard',
-  github: 'goldyard@qq.com',
+  github: 'ilongmao',
   linkedin: 'goldyard',
 
   // default notion icon and cover images for site-wide consistency (optional)
