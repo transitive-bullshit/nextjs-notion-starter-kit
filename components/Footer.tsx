dc601672e50522908cd09730bf3415c1ecb5c746
lizhimiao <zhi@miao.li>
2021-06-30 14:09:07 +0800

    added 不算子，disable LQIP

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -86,6 +86,8 @@ export const Footer: React.FC<{
 
   gtag('config', 'UA-147733158-1');`}
 </script>
+<script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
+<div className="bsz"><span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span></div>
     </footer>
   )
 }
