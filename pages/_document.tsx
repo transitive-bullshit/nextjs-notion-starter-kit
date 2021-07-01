4573d6f73378981f3b14e866a12040dbfa4a4251
lizhimiao <zhi@miao.li>
2021-07-01 11:59:49 +0800

    refactor typekit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -59,6 +59,19 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+
+    <script
+    dangerouslySetInnerHTML={{
+              __html: `
+          (function(d) {
+            var config = {
+              kitId: 'bhl5hda',
+              scriptTimeout: 3000,
+              async: true
+            },
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+          })(document); `}}
+        />
           </body>
         </Html>
       </IconContext.Provider>
