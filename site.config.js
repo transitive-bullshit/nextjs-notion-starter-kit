6a2eb990fc0973886a658e563451fcf23646d981
animatorplato <86768104+animatorplato@users.noreply.github.com>
2021-07-01 16:26:12 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '064603b9235642ed9a42e86c1d4af186',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'nicky_animator',
+  domain: 'https://github.com/animatorplato',
+  author: 'animtorplato',
 
   // open graph metadata (optional)
   description: 'Example site description',
