f7daf426c86a3dbd1c0b3d3eb2249841051c8776
ilongmao <31371881+ilongmao@users.noreply.github.com>
2021-07-05 17:22:26 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,8 +26,8 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author} 网站备案号: <a href="https://beian.miit.gov.cn">粤ICP备16129188号</a></div>
-
+      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+      <div className={styles.copyright}>网站备案号: <a href="https://beian.miit.gov.cn">粤ICP备16129188号</a></div>
       {hasMounted ? (
         <div className={styles.settings}>
           <a
