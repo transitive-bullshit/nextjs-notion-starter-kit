bf21bb92d258ceb27b8d9081c0a85bef3e40ad27
lizhimiao <zhi@miao.li>
2021-07-05 19:11:05 +0800

    fix homepage collection cover image size not changed.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -56,8 +56,7 @@ export default class MyDocument extends Document {
             gtag('config', 'UA-147733158-1', {
               page_path: window.location.pathname,
             });
-          `,
-            }}
+          `}}
           />
 
     <script
@@ -72,6 +71,12 @@ export default class MyDocument extends Document {
             h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
           })(document); `}}
         />
+         <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              resizeCover();`
+            }}
+          />
           </body>
         </Html>
       </IconContext.Provider>
