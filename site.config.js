433a04f6b14b108f920c5de7b28c65ad853b575b
ZION <sju0924@khu.ac.kr>
2021-07-23 21:16:41 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Blog-3fe590a6c59b4e8ab154a1496b656cdc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Gamestry',
+  domain: 'Gamest.ry',
+  author: 'khumembership',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'KHU-Gamestry',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
