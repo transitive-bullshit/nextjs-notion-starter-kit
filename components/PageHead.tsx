ded5e32770eca0d3cd4661875c9e0a17d75890e2
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-07-24 14:35:18 -0500

    fix header theme color based on dark mode

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -1,10 +1,19 @@
 import Head from 'next/head'
 import * as React from 'react'
 import * as types from 'lib/types'
+import useDarkMode from 'use-dark-mode'
 
 // TODO: remove duplication between PageHead and NotionPage Head
 
 export const PageHead: React.FC<types.PageProps> = ({ site }) => {
+  const darkMode = useDarkMode(false, { classNameDark: 'dark-mode' })
+  let themeColor
+  if (darkMode.value) {
+    themeColor = <meta name='theme-color' content='#06061a' />
+  } else {
+    themeColor = <meta name='theme-color' content='#fefefe' />
+  }
+
   return (
     <Head>
       <meta charSet='utf-8' />
@@ -13,16 +22,13 @@ export const PageHead: React.FC<types.PageProps> = ({ site }) => {
         name='viewport'
         content='width=device-width, initial-scale=1, shrink-to-fit=no'
       />
-
       {site?.description && (
         <>
           <meta name='description' content={site.description} />
           <meta property='og:description' content={site.description} />
         </>
       )}
-
-      <meta name='theme-color' content='#EB625A' />
-      <meta property='og:type' content='website' />
+      {themeColor}
     </Head>
   )
 }
