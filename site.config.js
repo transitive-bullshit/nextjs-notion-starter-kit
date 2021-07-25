5897fdb16b19ce997cd5baecbb66bd2ebc326ca3
Zealot <jaws02144@naver.com>
2021-07-25 20:08:40 +0900

    ss
    ss

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '3ede46aa4b6b40a6b994f470d57e1fec',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
