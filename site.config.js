012ba90159d1f6e499be129af24daa4cfe6b3c3d
yoshikimasubuchi <yoshixj4@gmail.com>
2021-08-05 01:02:22 +0900

    update site settings

--- site.config.js
+++ site.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // social usernames (optional)
   twitter: 'yoshixj',
   github: 'yoshixj',
-  linkedin: 'fisch2',
+  linkedin: 'yoshixj',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
