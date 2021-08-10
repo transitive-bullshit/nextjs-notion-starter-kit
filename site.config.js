5f28dd3cfee64c6b77dcff82b3903adedb7a8c2f
Leon <283585796@qq.com>
2021-08-10 19:30:25 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -32,7 +32,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: 'https://github.com/skefy/configs',
+  utterancesGitHubRepo: 'https://github.com/skefy/nextjs-notion-starter-kit',
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
