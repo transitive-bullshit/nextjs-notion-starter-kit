a60340921fb3de9e1d289ac6880546728df476d8
shueja-personal <j.a.shue@outlook.com>
2021-09-13 14:04:05 -0700

    Fix link

--- pages/wreath-countdown.tsx
+++ pages/wreath-countdown.tsx
@@ -1,6 +1,6 @@
 import Countdown from "react-countdown";
 const WreathCountdown= () => {
-    return <a href="https://sherwoodfundraiser.com/frc6995nomad/products"><div style={{display:"flex", alignItems:"center", justifyContent:"center", width:"100vw", height:"100vh", backgroundImage:`url('/wreath.jpg')`, backgroundRepeat:"no-repeat", backgroundSize:"auto 100%", backgroundPosition:"center", color:"white", fontSize:"12vw", fontWeight:"bolder"}}>
+    return <a href="https://sherwoodfundraiser.com/frc6995nomad/products" target="_top"><div style={{display:"flex", alignItems:"center", justifyContent:"center", width:"100vw", height:"100vh", backgroundImage:`url('/wreath.jpg')`, backgroundRepeat:"no-repeat", backgroundSize:"auto 100%", backgroundPosition:"center", color:"white", fontSize:"12vw", fontWeight:"bolder"}}>
         <div style={{textAlign:"center"}}>
             <Countdown  date={Date.UTC(2021,11,8,11,59,59)}></Countdown>
 <div style={{fontSize:"6vw"
