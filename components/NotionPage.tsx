b9fe0d6fc117b985d37fd6188b62b5a1627be829
shueja-personal <j.a.shue@outlook.com>
2021-09-13 20:34:50 -0700

    remove github button

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -291,8 +291,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
               />
             }
           />
-
-          <GitHubShareButton />
           {/*</TwitterContextProvider>*/}
         </div>
   )
