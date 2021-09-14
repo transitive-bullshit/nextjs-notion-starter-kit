2cf431c547a2d32e6de92cf2f746a8750ba8bbd2
Rayburn <46223530+AlanRayburn@users.noreply.github.com>
2021-09-14 15:36:34 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Alanrayburn-s-Blog-9e9cdb39016c4a30a14009db6b529a49',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'On The Way',
+  domain: 'https://onyx-astronomy-3cf.notion.site/Alanrayburn-s-Blog-9e9cdb39016c4a30a14009db6b529a49',
+  author: 'Alanrayburn',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'alanrayburn1',
+  github: 'alanrayburn',
+  
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
