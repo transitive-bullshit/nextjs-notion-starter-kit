a1dcdbc75360b1d868da9aaad2b8e08ffe6ac05b
shueja-personal <j.a.shue@outlook.com>
2021-09-13 20:31:06 -0700

    pull

--- pages/wreath-countdown.tsx
+++ pages/wreath-countdown.tsx
@@ -1,9 +1,71 @@
-import Countdown from "react-countdown";
-const WreathCountdown= () => {
-    return <a href="https://sherwoodfundraiser.com/frc6995nomad/products" target="_top"><div style={{display:"flex", alignItems:"center", justifyContent:"center", width:"100vw", height:"100vh", backgroundImage:`url('/wreath.jpg')`, backgroundRepeat:"no-repeat", backgroundSize:"auto 100%", backgroundPosition:"center", color:"white", fontSize:"12vw", fontWeight:"bolder"}}>
-        <div style={{textAlign:"center", backgroundColor:"#99cc33", borderRadius:"3vmin", padding:"5px"}}>
-            <Countdown  date={Date.UTC(2021,11,8,11,59,59)}></Countdown>
-<div style={{fontSize:"6vw"
-}}>Holiday Wreath Fundraiser<br />Click Here to Support NOMAD</div></div>
-</div></a>};
+import Countdown from 'react-countdown'
+
+const Completionist = () => (
+  <span>Thanks for Supporting our Wreath Fundraiser</span>
+)
+
+// Renderer callback with condition
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a completed state
+    return <Completionist />
+  } else {
+    // Render a countdown
+    return (
+      <>
+        <div style={{ display: 'flex', justifyContent: 'space-around', color:"black" }}>
+          <span>{days} days</span>
+          <span>{hours} hours</span>
+        </div>
+        <span>
+          Holiday Wreath Fundraiser
+          <br />
+          Click Here to Support NOMAD
+        </span>
+      </>
+    )
+  }
+}
+const WreathCountdown = (props) => {
+  return (
+    <a
+      href='https://sherwoodfundraiser.com/frc6995nomad/products'
+      target='_top'
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100vw',
+          height: '100vh',
+          backgroundImage: `url('/wreath.jpg')`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          boxShadow:"3px lightgrey",
+          
+          color: 'white',
+          fontSize: '10vmin',
+          fontWeight: 'lighter'
+        }}
+      >
+        <div
+          style={{
+            textAlign: 'center',
+            backgroundColor: '#99cc33',
+            borderRadius: '3px',
+            padding: '5px',
+            margin: '5vmin'
+          }}
+        >
+          <Countdown
+            date={Date.UTC(2021, 11, 8, 11, 59, 59)}
+            renderer={renderer}
+          ></Countdown>
+        </div>
+      </div>
+    </a>
+  )
+}
 export default WreathCountdown
