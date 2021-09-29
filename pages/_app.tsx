0608fb04c00761ad45e282afc4379fc63b6a9845
yoshikimasubuchi <yoshixj4@gmail.com>
2021-09-29 21:54:34 +0900

    fix style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,7 @@ import 'react-static-tweets/styles.css'
 
 // global style overrides for notion
 import 'styles/notion.css'
+import 'styles/notion_yoshixj.css'
 
 // global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
