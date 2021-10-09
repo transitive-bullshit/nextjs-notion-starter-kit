b7738b9533ac5f1d5f50ccfd85c57a0108d30d53
Rod <1449114627@qq.com>
2021-10-09 19:08:13 +0800

    home page1

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Infrod-64e2b28e2e994fb697f1df9e1adb876b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
