c2b0944ce49f57a629b9a26e9a0fbc0dafa12cf2
SoftLittleQian <61958492+SoftLittleQian@users.noreply.github.com>
2021-10-29 14:12:09 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '224f6eef96484bb48be31ebc5dec963d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'SoftLittleQian Blog',
+  domain: 'SoftLittleQian.it',
+  author: 'SLQ',
 
   // open graph metadata (optional)
   description: 'Example site description',
