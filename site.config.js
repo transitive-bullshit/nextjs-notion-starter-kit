89c0993f4246952b1b3c26e0edaeaed7a2641f96
Krutarth <35326083+krutarththakkar@users.noreply.github.com>
2021-10-31 17:42:23 +0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '50b6c552768d40a191d2f305544e0822',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Krutarth',
+  domain: 'bsk-club.vercel.app',
+  author: 'Krutarth',
 
   // open graph metadata (optional)
   description: 'Example site description',
