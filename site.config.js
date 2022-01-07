aa94168cdeabd3bfdc4b1cb039a9be63854b1f84
云中辞 <49399931+yunzhongci@users.noreply.github.com>
2022-01-07 13:21:15 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -33,7 +33,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: null,
+  utterancesGitHubRepo: yunzhongci/notion,
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
