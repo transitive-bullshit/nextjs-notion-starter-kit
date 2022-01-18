36612aea9a91f0bdc6efe3c0e55ef069985fd1ff
yunzhongci <yunzhongci@icloud.com>
2022-01-18 20:20:05 +0800

    注释机翻

--- site.config.js
+++ site.config.js
@@ -1,48 +1,48 @@
 module.exports = {
-  // where it all starts -- the site's root Notion page (required)
+  // 一切从这里开始——站点的根 Notion 页面（必需）
   rootNotionPageId: '666ffb1cba6b44f282bb88c17b35d7c1',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
+  // 如果要将页面限制为单个概念工作区（可选）
+  // （这应该是一个概念 ID；请参阅文档了解如何提取它）
   rootNotionSpaceId: "d2795493-807f-49f4-b577-22c42b882a79",
 
-  // basic site info (required)
+  // 基本站点信息（必填）
   name: '云中辞',
   domain: 'cinas.cn',
   author: '云中辞',
   beian: '浙ICP备2021036882号',
 
-  // open graph metadata (optional)
+  // 站点元数据（可选）
   description: '云中谁寄锦书来',
   socialImageTitle: '云中辞',
   socialImageSubtitle: '雁字回时 月满西楼',
 
-  // social usernames (optional)
+  // 社交用户名（可选）
   // twitter: 'transitive_bs',
   // github: 'transitive-bullshit',
   // linkedin: 'fisch2',
-
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
+  
+  // 默认notion 图标和 封面图像 （可选）
+  // 特定页面的值将覆盖这些站点的默认值
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
-  // image CDN host to proxy all image requests through (optional)
-  // NOTE: this requires you to set up an external image proxy
+  // 图片 CDN 主机通过代理所有图片请求（可选）
+  // 注意：这需要您设置外部图像代理
   imageCDNHost: null,
 
-  // Utteranc.es comments via GitHub issue comments (optional)
+  // 通过 GitHub 的 issue 发布评论，使用 Utteranc.es 实现这个功能（可选）
   utterancesGitHubRepo: 'yunzhongci/notion',
 
-  // whether or not to enable support for LQIP preview images (optional)
-  // NOTE: this requires you to set up Google Firebase and add the environment
-  // variables specified in .env.example
+  // 是否开启对 LQIP 预览图片的支持（可选）
+  // 注意：这需要您设置 Google Firebase 并添加环境
+  // .env.example 中指定的变量
   isPreviewImageSupportEnabled: false,
 
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
+  // notion 页面 ID 到 URL 路径的映射（可选）
+  // 此处定义的任何页面都将覆盖其默认 URL 路径
+  // 例子：
   //
   // pageUrlOverrides: {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
