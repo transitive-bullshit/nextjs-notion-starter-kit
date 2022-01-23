0a021c7ea0fe2954def8a355e9e6177893510e93
liu.zhen <liu.zhen@xsky.com>
2022-01-24 00:01:41 +0800

    initial
    Signed-off-by: liu.zhen <liu.zhen@xsky.com>

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'e2aac0674a514dfd8bbfbe974ab901e2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'AC-Ring',
+  domain: 'acring.cn',
+  author: 'Liu Zhen',
 
   // open graph metadata (optional)
   description: 'Example site description',
