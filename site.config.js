0c7a0a621db15d1f939269bc2eb63a3489a23255
german23405 <68710913+german23405@users.noreply.github.com>
2022-02-02 21:26:07 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'faac5ed1b8b94947acba49d5aaaf8c9b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Pownery - Product Owner Space',
+  domain: 'pownery.com',
+  author: 'Herman Baiatian',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Everything about Product Ownership, Management and Business Analysis',
+  socialImageTitle: 'Pownery.com',
+  socialImageSubtitle: 'Hey, POs👋',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
