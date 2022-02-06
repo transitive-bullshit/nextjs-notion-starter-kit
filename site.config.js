44aada4cb2729f38b6a247d3663aad314af9d96f
leothesen <leothesen@gmail.com>
2022-02-06 11:25:11 +0200

    updated site config variables 🙃

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '2a9bf7526da84f7daa846a866faf1799',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Leo Thesen',
+  domain: 'leothesen.com',
+  author: 'Leo Thesen',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Leo Thesen\'s personal website.',
+  socialImageTitle: 'Leo Thesen',
+  socialImageSubtitle: 'Personal website',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'leothesen',
+  github: 'leothesen',
+  linkedin: 'leothesen',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
