80067a25edc55e176134bc86c80097f417c32091
leothesen <leothesen@gmail.com>
2022-02-06 16:55:29 +0200

    removed the big ugly Github thingy 🧐

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -280,7 +280,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         }
       />
 
-      <GitHubShareButton />
+      {/* <GitHubShareButton /> */}
     </TwitterContextProvider>
   )
 }
