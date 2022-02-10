bc26fbef24b780a37a16ef382e312facb138bcf4
Fuhuang <fuhuang@wenbolong.com>
2022-02-10 19:19:34 +0800

    Update 部件/Footer.tsx

--- 部件/Footer.tsx
+++ 部件/Footer.tsx
@@ -26,10 +26,8 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      //  <div className={styles.copyright}>Copyright 2021 {config.author}</div>
-      <div className={styles.copyright}>
-      <a href="https://beian.miit.gov.cn/" target="_blank">浙B2-20080101</a>
-      </div>
+      <div className={styles.copyright}><a href="https://beian.miit.gov.cn/">浙B2-20080101 {config.author}</a></div>
+     
       {hasMounted ? (
         <div className={styles.settings}>
           <a
