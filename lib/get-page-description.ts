7405a8d2b94b180bfdaeb737db199d40423a7898
Julien Bras <julienbras@sidoine.org>
2022-02-24 20:40:22 -0500

    fix: build failure when no page property Description on a collection

--- lib/get-page-description.ts
+++ lib/get-page-description.ts
@@ -5,5 +5,9 @@ export function getPageDescription(
   block: types.Block,
   recordMap: types.ExtendedRecordMap
 ): string | null {
-  return getPageProperty('Description', block, recordMap)
+  try {
+    return getPageProperty('Description', block, recordMap)
+  } catch (err) {
+    return null
+  }
 }
