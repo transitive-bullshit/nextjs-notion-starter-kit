9650b7af38977da5a8befc33d628a7428c97c5ac
卢东东 <xjoylu@gmail.com>
2022-03-18 14:13:00 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // social usernames (optional)
   github: 'xjoylu',
   twitter:'xjoylu',
-  linkedin:'ludongdong',
+  linkedin:'',
   
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
