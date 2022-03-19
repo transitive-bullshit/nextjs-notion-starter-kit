2241b7515bb9309cbd716a4cd06c1063ffb3e9a0
l7wei <87221670+l7wei@users.noreply.github.com>
2022-03-19 17:07:06 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'e932a74f50d9451b8287f54d4242246c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
