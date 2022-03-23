91ad272d296693c856934b92776e20a765989282
l7wei <87221670+l7wei@users.noreply.github.com>
2022-03-23 20:05:12 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: 'Notion-78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '島島阿學',
-  domain: 'article.daoedu.tw',
-  author: '島島阿學學習社群',
+  name: 'Transitive Bullshit',
+  domain: 'transitivebullsh.it',
+  author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: '島島阿學',
-  socialImageTitle: '島島阿學',
-  socialImageSubtitle: '島島阿學 👋',
+  description: 'Example site description',
+  socialImageTitle: 'Transitive Bullshit',
+  socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
