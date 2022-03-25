7172dd0457fe076bc30c37c33ddda0709fc0e806
Travis Fischer <fisch0920@gmail.com>
2022-03-25 08:28:30 -0400

    fix: new import paths for third-party components

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -42,27 +42,24 @@ import styles from './styles.module.css'
 // -----------------------------------------------------------------------------
 
 const Code = dynamic(() =>
-  import('react-notion-x/build/esm/third-party/code').then((m) => m.Code)
+  import('react-notion-x/build/third-party/code').then((m) => m.Code)
 )
 const Collection = dynamic(() =>
-  import('react-notion-x/build/esm/third-party/collection').then(
+  import('react-notion-x/build/third-party/collection').then(
     (m) => m.Collection
   )
 )
 const Equation = dynamic(() =>
-  import('react-notion-x/build/esm/third-party/equation').then(
-    (m) => m.Equation
-  )
+  import('react-notion-x/build/third-party/equation').then((m) => m.Equation)
 )
 const Pdf = dynamic(
-  () => import('react-notion-x/build/esm/third-party/pdf').then((m) => m.Pdf),
+  () => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf),
   {
     ssr: false
   }
 )
 const Modal = dynamic(
-  () =>
-    import('react-notion-x/build/esm/third-party/modal').then((m) => m.Modal),
+  () => import('react-notion-x/build/third-party/modal').then((m) => m.Modal),
   {
     ssr: false
   }
