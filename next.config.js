e783b3dac75aab651c61d0793f45fe309589e824
Travis Fischer <fisch0920@gmail.com>
2022-03-25 09:33:42 -0400

    🤦

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-import withBundleAnalyzer from '@next/bundle-analyzer'
-
-export default withBundleAnalyzer({
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
-})({
+})
+
+module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
   images: {
     domains: [
