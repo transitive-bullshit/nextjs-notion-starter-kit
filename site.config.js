74391bab90fc958945a053144314a4af8a128a6b
Travis Fischer <fisch0920@gmail.com>
2022-03-26 03:12:54 -0400

    🥜

--- site.config.js
+++ site.config.js
@@ -28,7 +28,7 @@ export default {
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: false,
+  isPreviewImageSupportEnabled: true,
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
