debc7fc1a656a7128887e713eaf2eb5d93acc82c
Travis Fischer <fisch0920@gmail.com>
2022-03-29 11:30:01 -0400

    switch default notion ID to 7875426197cf461698809def95960ebf

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 export default {
-  // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  // the site's root Notion page (required)
+  rootNotionPageId: '7875426197cf461698809def95960ebf',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
