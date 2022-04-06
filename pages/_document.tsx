e0deeea9b1c7fd65d03e6ad9e3503d365ae295a6
Travis Fischer <fisch0920@gmail.com>
2022-04-06 19:31:44 -0400

    feat: clean up default favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,30 +8,12 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.png' />
-
-            <link
-              rel='apple-touch-icon'
-              sizes='180x180'
-              href='/apple-touch-icon.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='96x96'
-              href='/favicon-96x96.png'
-            />
+            <link rel='shortcut icon' href='/favicon.ico' />
             <link
               rel='icon'
               type='image/png'
               sizes='32x32'
-              href='/favicon-32x32.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='16x16'
-              href='/favicon-16x16.png'
+              href='favicon.png'
             />
 
             <link rel='manifest' href='/manifest.json' />
