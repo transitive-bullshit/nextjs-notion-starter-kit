cda31eb17852367235bf9c7e0ee5eedac3297914
Liona <lionacc@163.com>
2022-04-08 20:49:25 +0800

    update self config

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,6 +11,7 @@ import * as config from 'lib/config'
 import styles from './styles.module.css'
 
 // TODO: merge the data and icons from PageSocial with the social links in Footer
+const year = new Date().getFullYear()
 
 export const FooterImpl: React.FC = () => {
   const [hasMounted, setHasMounted] = React.useState(false)
@@ -30,7 +31,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>Copyright {year} {config.author}</div>
 
       <div className={styles.settings}>
         {hasMounted && (
--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,22 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '858eee0262e740148d2591d9acb61fe7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: "西片 Hack The Box",
+  domain: 'notion.allinu.vercel.app',
+  author: '西片',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '和你分享 Hack The Box 相关内容',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  github: 'allinu',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
