a1d9a074b61744c4adca6e5290dfb011b4fc63a0
yanglin <ss880309@gmail.com>
2022-04-13 21:46:10 +0800

    更新id

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 export default {
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '03c13ba886b94ba293054babac17a3d0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
