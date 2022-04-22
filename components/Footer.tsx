c0f61dbbb15d3f99f55789c0a2f676b7a340d250
ubq-cw <chris.wu.dev@gmail.com>
2022-04-22 14:01:33 -0400

    add dynamic year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>Copyright {new Date().getFullYear()} {config.author}</div>
 
       {hasMounted ? (
         <div className={styles.settings}>
