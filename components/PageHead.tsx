4ae14b182926ceaee6eb41dc403c7698ae2f0ab2
Travis Fischer <fisch0920@gmail.com>
2022-04-22 17:39:17 -0400

    🏨

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -13,7 +13,7 @@ export const PageHead: React.FC<
     url?: string
   }
 > = ({ site, title, description, pageId, image, url }) => {
-  const rssFeedUrl = `${config.host}/feed.xml`
+  const rssFeedUrl = `${config.host}/feed`
 
   title = title ?? site?.name
   description = description ?? site?.description
