2b9a457ac724af6bb14186f4f9422f9f42e710ab
アレクサンダー.eth <gpg@pavlovcik.com>
2022-05-01 16:18:52 +0800

    update(icons): cyan-teal finish

--- public/sizes/resizer.sh
+++ public/sizes/resizer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Dependency: ImageMagick
+# Installation: brew install imagemagick
+
 # Usage: ./resize_many.sh <filename> <sizes...>
 # e.g. ./resize_many.sh myasset.png 10x10 20x20 30x30
 #      produces 3 files, myasset10x10.png myasset20x20.png myasset30x30.png
