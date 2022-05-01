f9f226d91f2b582a2f0be9f9719d0c1177ce464d
アレクサンダー.eth <gpg@pavlovcik.com>
2022-05-01 17:02:17 +0800

    fix(video): only on root

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -31,8 +31,6 @@ import { Page404 } from './Page404'
 import { PageHead } from './PageHead'
 import { PageActions } from './PageActions'
 import { Footer } from './Footer'
-import { PageSocial } from './PageSocial'
-import { GitHubShareButton } from './GitHubShareButton'
 
 import styles from './styles.module.css'
 
@@ -145,27 +143,35 @@ export const NotionPage: React.FC<types.PageProps> = ({
 
   const heroVideo =
     'https://storageapi.fleek.co/2e62e11d-d4be-4c6f-a2bb-b159c83a0d95-bucket/ubq.fi/hero.mp4'
-  const pageCover = (
-    <>
-      <div className='notion-page-cover-background'></div>
-      <video
-        className='notion-page-cover'
-        autoPlay
-        controls={false}
-        loop
-        muted
-        playsInline
-        // // poster={poster}
-        // src={heroVideo}
-      >
-        <source src={heroVideo} type='video/mp4' />
-      </video>
-    </>
-  )
+
+  let pageCover
 
   if (isRootPage) {
-    // const poster =
-    // 'https://ssfy.io/https%3A%2F%2Fwww.notion.so%2Fimage%2Fhttps%253A%252F%252Fs3-us-west-2.amazonaws.com%252Fsecure.notion-static.com%252Fbb144e8e-3a57-4e68-b2b9-6a80dbff07d0%252FGroup_3.png%3Ftable%3Dblock%26id%3Dff1a3cae-9009-41e4-9cc4-d4458cc2867d%26cache%3Dv2'
+    // HERO VIDEO
+    pageCover = (
+      <>
+        <div className='notion-page-cover-background'></div>
+        <video
+          className='notion-page-cover'
+          autoPlay
+          controls={false}
+          loop
+          muted
+          playsInline
+          // // poster={poster}
+          // src={heroVideo}
+        >
+          <source src={heroVideo} type='video/mp4' />
+        </video>
+      </>
+    )
+  } else {
+    // NO HERO VIDEO
+    pageCover = (
+      <>
+        <div className='notion-page-cover-background'></div>
+      </>
+    )
   }
 
   return (
