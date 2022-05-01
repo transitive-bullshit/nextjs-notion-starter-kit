99fb293797963117c94747ba909d428b8f061b42
アレクサンダー.eth <gpg@pavlovcik.com>
2022-05-01 17:02:01 +0800

    fix(footer): copyright unicode symbol

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright {new Date().getFullYear()} {config.author}</div>
+      <div className={styles.copyright}>&copy; {new Date().getFullYear()} {config.author}</div>
 
       {hasMounted ? (
         <div className={styles.settings}>
