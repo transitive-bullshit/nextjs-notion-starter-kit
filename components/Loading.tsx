771a14e54cf6c37ced737c4f25d495cc40ce71e5
アレクサンダー.eth <gpg@pavlovcik.com>
2022-05-05 00:46:05 +0800

    enhancement(errors): let user access source notion as a backup plan

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -5,6 +5,18 @@ import styles from './styles.module.css'
 
 export const Loading: React.FC = () => (
   <div className={styles.container}>
+    <p id='loading-message'>
+      Fetching the latest contents from the&nbsp;
+      <a
+        href='https://ubiquitydao.notion.site/Ubiquity-DAO-ff1a3cae900941e49cc4d4458cc2867d'
+        target='_blank'
+        rel='noreferrer'
+      >
+        DAO&apos;s Notion
+      </a>
+      .
+    </p>
+    &nbsp;
     <LoadingIcon />
   </div>
 )
