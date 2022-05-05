19765cac7ba3347bb774d97cb76e598524b94d53
Leigh Johnson <leigh@bitsy.ai>
2022-05-06 00:33:41 +0200

    update site config

--- site.config.ts
+++ site.config.ts
@@ -2,28 +2,28 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Print-Nanny-Wiki-ac1079fafc944d769aa21cf8bffe4692',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'PrintNanny.ai Wiki',
+  domain: 'wiki.printnanny.ai',
+  author: 'Leigh Johnson',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Getting Started Guide & FAQ for PrintNanny.ai',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'grepLeigh',
+  github: 'leigh-johnson',
+  linkedin: 'www.linkedin.com/in/leigh-by-the-bay',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: 'purpleheart',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
