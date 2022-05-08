7b49cc1eb9edb84ed89619c017638e0ea8314d99
zhiyicai <2857483@qq.com>
2022-05-08 15:53:54 +0200

    GitHubShareButton

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -29,7 +29,7 @@ import { PageHead } from './PageHead'
 import { PageAside } from './PageAside'
 import { Footer } from './Footer'
 import { NotionPageHeader } from './NotionPageHeader'
-import { GitHubShareButton } from './GitHubShareButton'
+// import { GitHubShareButton } from './GitHubShareButton'
 
 import styles from './styles.module.css'
 
