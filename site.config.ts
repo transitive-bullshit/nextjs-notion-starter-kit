b9cfd3ececbc6f2877d2cca27b02ecb5280f1540
Mancuoj <45707684+mancuoj@users.noreply.github.com>
2022-05-11 14:09:17 +0800

    feat: update config

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2022-099fa0d1f30e4f549e289f6ce916ad67',
 
+  // https://mancuoj.notion.site/2022-099fa0d1f30e4f549e289f6ce916ad67
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '2022',
+  domain: 'https://mancuoj.notion.site/2022-099fa0d1f30e4f549e289f6ce916ad67',
+  author: 'Mancuoj',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Mancuoj's 2022 Blog',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'mancuoj',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
