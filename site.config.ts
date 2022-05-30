16742a0a955b5a7c2cd07b4c98d7767f7d00ddd6
Wei <87221670+l7wei@users.noreply.github.com>
2022-05-30 20:00:19 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: 'cc246794-7553-4039-870c-c2c6a9f8af8e',
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '島島阿學',
+  domain: 'blog.daoedu.tw',
+  author: '島島阿學',
 //
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'daodaoedu',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
