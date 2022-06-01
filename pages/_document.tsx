331e8097e77bf4154c95540e5d0ff97595d6ddfc
XueRainey <xuebing_orz@qq.com>
2022-06-01 11:17:54 +0800

    add:baidu-statistical

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,41 +23,54 @@ export default class MyDocument extends Document {
             <script
               dangerouslySetInnerHTML={{
                 __html: `
-/** Inlined version of noflash.js from use-dark-mode */
-;(function () {
-  var storageKey = 'darkMode'
-  var classNameDark = 'dark-mode'
-  var classNameLight = 'light-mode'
-  function setClassOnDocumentBody(darkMode) {
-    document.body.classList.add(darkMode ? classNameDark : classNameLight)
-    document.body.classList.remove(darkMode ? classNameLight : classNameDark)
-  }
-  var preferDarkQuery = '(prefers-color-scheme: dark)'
-  var mql = window.matchMedia(preferDarkQuery)
-  var supportsColorSchemeQuery = mql.media === preferDarkQuery
-  var localStorageTheme = null
-  try {
-    localStorageTheme = localStorage.getItem(storageKey)
-  } catch (err) {}
-  var localStorageExists = localStorageTheme !== null
-  if (localStorageExists) {
-    localStorageTheme = JSON.parse(localStorageTheme)
-  }
-  // Determine the source of truth
-  if (localStorageExists) {
-    // source of truth from localStorage
-    setClassOnDocumentBody(localStorageTheme)
-  } else if (supportsColorSchemeQuery) {
-    // source of truth from system
-    setClassOnDocumentBody(mql.matches)
-    localStorage.setItem(storageKey, mql.matches)
-  } else {
-    // source of truth from document.body
-    var isDarkMode = document.body.classList.contains(classNameDark)
-    localStorage.setItem(storageKey, JSON.stringify(isDarkMode))
-  }
-})();
-`
+                  /** Inlined version of noflash.js from use-dark-mode */
+                  ;(function () {
+                    var storageKey = 'darkMode'
+                    var classNameDark = 'dark-mode'
+                    var classNameLight = 'light-mode'
+                    function setClassOnDocumentBody(darkMode) {
+                      document.body.classList.add(darkMode ? classNameDark : classNameLight)
+                      document.body.classList.remove(darkMode ? classNameLight : classNameDark)
+                    }
+                    var preferDarkQuery = '(prefers-color-scheme: dark)'
+                    var mql = window.matchMedia(preferDarkQuery)
+                    var supportsColorSchemeQuery = mql.media === preferDarkQuery
+                    var localStorageTheme = null
+                    try {
+                      localStorageTheme = localStorage.getItem(storageKey)
+                    } catch (err) {}
+                    var localStorageExists = localStorageTheme !== null
+                    if (localStorageExists) {
+                      localStorageTheme = JSON.parse(localStorageTheme)
+                    }
+                    // Determine the source of truth
+                    if (localStorageExists) {
+                      // source of truth from localStorage
+                      setClassOnDocumentBody(localStorageTheme)
+                    } else if (supportsColorSchemeQuery) {
+                      // source of truth from system
+                      setClassOnDocumentBody(mql.matches)
+                      localStorage.setItem(storageKey, mql.matches)
+                    } else {
+                      // source of truth from document.body
+                      var isDarkMode = document.body.classList.contains(classNameDark)
+                      localStorage.setItem(storageKey, JSON.stringify(isDarkMode))
+                    }
+                  })();
+                  `
+              }}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  var _hmt = _hmt || [];
+                  (function() {
+                    var hm = document.createElement("script");
+                    hm.src = "https://hm.baidu.com/hm.js?5d4604ea78dcd87205ab8123de01b01e";
+                    var s = document.getElementsByTagName("script")[0]; 
+                    s.parentNode.insertBefore(hm, s);
+                  })();
+                `
               }}
             />
             <Main />
