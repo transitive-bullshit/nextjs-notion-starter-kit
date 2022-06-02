55640ccc9cd2f2c7e2350ac55d70b69a33c8576a
XueRainey <xuebing_orz@qq.com>
2022-06-02 11:20:48 +0800

    open-reids-mode

--- site.config.ts
+++ site.config.ts
@@ -34,7 +34,7 @@ export default siteConfig({
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: false,
+  isRedisEnabled: true,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
