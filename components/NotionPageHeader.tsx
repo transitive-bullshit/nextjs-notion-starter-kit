b4f4a09ea8d277a36d71804463d21b0ed86e039b
XueRainey <xuebing_orz@qq.com>
2022-06-02 10:09:55 +0800

    update:header

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 import cs from 'classnames'
+import Link from 'next/link'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { Header, Breadcrumbs, Search, useNotionContext } from 'react-notion-x'
 import * as types from 'notion-types'
+import * as config from 'lib/config'
 
 import { useDarkMode } from 'lib/use-dark-mode'
 import { navigationStyle, navigationLinks, isSearchEnabled } from 'lib/config'
@@ -44,8 +46,13 @@ export const NotionPageHeader: React.FC<{
   return (
     <header className='notion-header'>
       <div className='notion-nav-header'>
-        <Breadcrumbs block={block} rootOnly={true} />
-
+        {/* <Breadcrumbs block={block} rootOnly={true} /> */}
+        <div className="breadcrumbs">
+          <a className="breadcrumb" href='/'>
+            <img src="/favicon.png" style={{ width: 22, height: 22, marginRight: 6, borderRadius: 2 }} />
+            {config.name}
+          </a>
+        </div>
         <div className='notion-nav-header-rhs breadcrumbs'>
           {navigationLinks
             ?.map((link, index) => {
