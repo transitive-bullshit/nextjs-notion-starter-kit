1ac4b67493cef632b1dfcbc7fd235eff0094318d
Richard Song <76866748+RichardS0268@users.noreply.github.com>
2022-06-03 10:59:59 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -29,12 +29,12 @@ export default siteConfig({
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+  isPreviewImageSupportEnabled: false,
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: true,
+  isRedisEnabled: false,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
