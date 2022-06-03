bb8b03bff16da67c90443294e4bbc1210bed5c23
Richard Song <76866748+RichardS0268@users.noreply.github.com>
2022-06-03 20:20:14 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/RichardS0268/nextjs-notion-starter-kit'
+      href='https://github.com/RichardS0268/blog'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
