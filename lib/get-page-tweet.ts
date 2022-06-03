978dd72eb4b9615bf53cce43f59c523a3492fb49
Richard Song <76866748+RichardS0268@users.noreply.github.com>
2022-06-03 10:54:59 +0800

    Delete lib directory

--- lib/get-page-tweet.ts
+++
@@ -1,9 +0,0 @@
-import * as types from './types'
-import { getPageProperty } from 'notion-utils'
-
-export function getPageTweet(
-  block: types.Block,
-  recordMap: types.ExtendedRecordMap
-): string | null {
-  return getPageProperty('Tweet', block, recordMap)
-}
--- lib/notion-api.ts
+++
@@ -1,5 +0,0 @@
-import { NotionAPI } from 'notion-client'
-
-export const notion = new NotionAPI({
-  apiBaseUrl: process.env.NOTION_API_BASE_URL
-})
