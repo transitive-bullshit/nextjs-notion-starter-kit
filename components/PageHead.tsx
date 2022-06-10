4df93c6ea4e808db8899f61ba2de8dac90822fc0
si1k <19499950+si1k@users.noreply.github.com>
2022-06-10 10:48:26 -0400

    Adding meta tags to remove bars on safari browser

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -33,7 +33,11 @@ export function PageHead({
       />
 
       <meta name='mobile-web-app-capable' content='yes' />
-      <meta name='apple-mobile-web-app-status-bar-style' content='black' />
+      <meta name='apple-mobile-web-app-capable' content='yes' />
+      <meta
+        name='apple-mobile-web-app-status-bar-style'
+        content='black-translucent'
+      />
 
       <meta
         name='theme-color'
