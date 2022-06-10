64de08a6192064a02e21c20ace1185faaa502e64
si1k <19499950+si1k@users.noreply.github.com>
2022-06-10 11:35:15 -0400

    Updating bar style to black

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -36,7 +36,7 @@ export function PageHead({
       <meta name='apple-mobile-web-app-capable' content='yes' />
       <meta
         name='apple-mobile-web-app-status-bar-style'
-        content='black-translucent'
+        content='black'
       />
 
       <meta
