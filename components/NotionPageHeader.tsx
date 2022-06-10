732b7c27e887eb21ded232dfde6b0d09854bb04c
si1k <19499950+si1k@users.noreply.github.com>
2022-06-10 17:49:49 -0400

    Update html element

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -62,7 +62,7 @@ export const HeaderLogo: React.FC<{
         </components.PageLink>
       )
     }
-    return <a style={{ padding: 12, display: 'flex' }}>{props.children}</a>
+    return <div style={{ padding: 12, display: 'flex' }}>{props.children}</div>
   }
 
   return (
