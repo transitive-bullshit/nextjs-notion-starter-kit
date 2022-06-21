4c8d04c4516d60b96af0acbc19224b923dd33346
leothesen <leothesen@gmail.com>
2022-06-21 21:41:07 +0200

    [ADDED] thank you to TBS

--- lib/bootstrap-client.ts
+++ lib/bootstrap-client.ts
@@ -1,6 +1,6 @@
 export function bootstrap() {
   console.log(`
-
+  
   LL                    
   LL        eee   oooo  
   LL      ee   e oo  oo 
@@ -8,5 +8,6 @@ export function bootstrap() {
   LLLLLLL  eeeee  oooo    
                                                                                                
    This site is built using Notion, Next.js, and https://github.com/NotionX/react-notion-x.
+   All credit goes to https://github.com/transitive-bullshit/nextjs-notion-starter-kit.
 `)
 }
