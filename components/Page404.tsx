aaf7ebbccce1e8fcc6501e2fb391da1b28bf24d4
leothesen <leothesen@gmail.com>
2022-07-01 22:36:55 +0200

    added contact to 404 page

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -15,6 +15,8 @@ export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
         <main className={styles.main}>
           <h1>Page Not Found</h1>
 
+          <a href="https://us14.list-manage.com/contact-form?u=48ac2df5c8c9410ed02b4c867&form_id=b38cf72feb797a9ebd3b251645784085">Let Leo know that something went wrong</a>
+
           {error ? (
             <p>{error.message}</p>
           ) : (
@@ -22,7 +24,6 @@ export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
               <p>
                 Make sure that Notion page &quot;{pageId}&quot; is publicly
                 accessible.
-                <a href="https://us14.list-manage.com/contact-form?u=48ac2df5c8c9410ed02b4c867&form_id=b38cf72feb797a9ebd3b251645784085">Let Leo know that something went wrong</a>
               </p>
             )
           )}
