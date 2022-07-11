cb4de4c00b78a04daa68c8de53e8d55ff9d94263
ndi8093 <nd17id@brocku.ca>
2022-07-11 10:14:37 -0400

    Update NotionPageHeader.tsx

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import cs from 'classnames'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
-import { Header, Breadcrumbs, Search, useNotionContext } from 'react-notion-x'
+import { Header, Search, useNotionContext } from 'react-notion-x'
 import * as types from 'notion-types'
 
 import { useDarkMode } from 'lib/use-dark-mode'
@@ -44,7 +44,7 @@ export const NotionPageHeader: React.FC<{
   return (
     <header className='notion-header'>
       <div className='notion-nav-header'>
-        <Breadcrumbs block={block} rootOnly={true} />
+        
 
         <div className='notion-nav-header-rhs breadcrumbs'>
           {navigationLinks
