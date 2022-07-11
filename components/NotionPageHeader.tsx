c1077c2d321df4e5f03a9c38ad66683ff99176b5
ndi8093 <nd17id@brocku.ca>
2022-07-10 23:27:08 -0400

    Fix bugs

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -44,6 +44,43 @@ export const NotionPageHeader: React.FC<{
   return (
     <header className='notion-header'>
       <div className='notion-nav-header'>
+        <Breadcrumbs block={block} rootOnly={true} />
+
+        <div className='notion-nav-header-rhs breadcrumbs'>
+          {navigationLinks
+            ?.map((link, index) => {
+              if (!link.pageId && !link.url) {
+                return null
+              }
+
+              if (link.pageId) {
+                return (
+                  <components.PageLink
+                    href={mapPageUrl(link.pageId)}
+                    key={index}
+                    className={cs(styles.navLink, 'breadcrumb', 'button')}
+                  >
+                    {link.title}
+                  </components.PageLink>
+                )
+              } else {
+                return (
+                  <components.Link
+                    href={link.url}
+                    key={index}
+                    className={cs(styles.navLink, 'breadcrumb', 'button')}
+                  >
+                    {link.title}
+                  </components.Link>
+                )
+              }
+            })
+            .filter(Boolean)}
+
+          <ToggleThemeButton />
+
+          {isSearchEnabled && <Search block={block} title={null} />}
+        </div>
       </div>
     </header>
   )
