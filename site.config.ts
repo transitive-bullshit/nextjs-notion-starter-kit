ef652a5ed9705ebbe59f0bcfa88736af53530d3d
linwentao <linwentao@saicmobility.com>
2022-07-21 13:02:55 +0800

    linteresting

--- site.config.ts
+++ site.config.ts
@@ -2,30 +2,30 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'ac7732257a34422896b61bf963ca439a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Linteresting',
+  domain: 'linteresting.notion.site/Linteresting-ac7732257a34422896b61bf963ca439a',
+  author: 'Lin',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'Linterestingg',
+  github: 'Linteresting',
+  linkedin: '',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: 'https://sm.ms/image/fI3kmQyYzM8tiPl',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
