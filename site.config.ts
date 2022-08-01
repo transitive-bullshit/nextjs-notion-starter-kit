a94f75f6fb9799caed17583b73b3cebb4534f1e3
Bonous <79294077+Bonous@users.noreply.github.com>
2022-08-01 13:34:03 -0400

    Working on default template

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '2cbf40a4fdd14de2bb2ae17ede00469f',
+  rootNotionPageId: '2d9e9cfffd214950a59e317cd6806c0d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
