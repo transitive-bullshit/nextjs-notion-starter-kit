84903b929de7c25305034109722e800580aa3bf2
Bonous <79294077+Bonous@users.noreply.github.com>
2022-08-01 13:05:25 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '2cbf40a4fdd14de2bb2ae17ede00469f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Simon Gorbet',
+  domain: 'simongorbet.xyz',
+  author: 'Simon Gorbet',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Personal website for Simon Gorbet',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
