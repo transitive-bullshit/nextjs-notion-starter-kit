07240bbb8256a9bc780f7025925ae4430168a090
DonetskRiver <aiqiaochu@gmail.com>
2022-08-23 15:19:24 +0800

    Update site.config.ts
    首次创建

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'c2dfc06de2a24efdad18b65afe9a9352',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Tsao',
+  domain: 'cycinc.eu.org',
+  author: 'Tsao',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '南山牧场',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
