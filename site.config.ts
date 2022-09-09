60cc4b43898c2ca7694a38b5bf604eba2fbcf8b1
Soxhwhat <52189710+soxhwhat@users.noreply.github.com>
2022-09-09 12:40:22 +0800

    change: notion refresh account

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'e7f13a8f093345189ab4e3ea0a30e25e',
+  rootNotionPageId: 'A-e7f13a8f093345189ab4e3ea0a30e25e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: '起身独立向荒原',
-  domain: 'https://www.notion.so/e7f13a8f093345189ab4e3ea0a30e25e',
+  domain: 'https://www.notion.so/A-e7f13a8f093345189ab4e3ea0a30e25e',
   author: 'Soxhwhat',
 
   // open graph metadata (optional)
