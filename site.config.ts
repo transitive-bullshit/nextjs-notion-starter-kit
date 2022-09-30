5fa88410a72729620e00583b9f0e563b01162cdb
HQhanqiZHQ <59696760+HQhanqiZHQ@users.noreply.github.com>
2022-09-29 21:42:34 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
 //   twitter: '',
   github: 'HQhanqiZHQ',
-  linkedin: 'Hanqi Z',
+  linkedin: 'Hanna Z',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
