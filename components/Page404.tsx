947e68aced03994d02ed6a551a47b268d0b2c5e0
ryzhak <ryzhak.vladimir@gmail.com>
2022-11-09 14:32:44 +0300

    feat: add noreferrer to links in 404 page

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -28,22 +28,22 @@ export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
               </p>
               <ul>
                 <li>
-                  <a href="//github.com/ubiquity/ubiquity-dollar/wiki" target="_blank">Docs</a>
+                  <a href="//github.com/ubiquity/ubiquity-dollar/wiki" target="_blank" rel="noreferrer">Docs</a>
                 </li>
                 <li>
-                  <a href="//dao.ubq.fi/faq" target="_blank">FAQ</a>
+                  <a href="//dao.ubq.fi/faq" target="_blank" rel="noreferrer">FAQ</a>
                 </li>
                 <li>
-                  <a href="//github.com/ubiquity/ubiquity-dollar" target="_blank">Github</a>
+                  <a href="//github.com/ubiquity/ubiquity-dollar" target="_blank" rel="noreferrer">Github</a>
                 </li>
                 <li>
-                  <a href="//discord.gg/SjymJ5maJ4" target="_blank">Discord</a>
+                  <a href="//discord.gg/SjymJ5maJ4" target="_blank" rel="noreferrer">Discord</a>
                 </li>
                 <li>
-                  <a href="//t.me/ubiquitydao" target="_blank">Telegram</a>
+                  <a href="//t.me/ubiquitydao" target="_blank" rel="noreferrer">Telegram</a>
                 </li>
                 <li>
-                  <a href="//twitter.com/UbiquityDAO" target="_blank">Twitter</a>
+                  <a href="//twitter.com/UbiquityDAO" target="_blank" rel="noreferrer">Twitter</a>
                 </li>
               </ul>
             </div>
