ace88f691ecb30ff8e6487d4c84ff324538b18b6
Steven Rugg <Steven.rugg@outlook.com>
2022-11-10 04:37:42 -0800

    changed environmental variables

--- site.config.ts
+++ site.config.ts
@@ -2,31 +2,31 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'd429db03e0a64c92b319ff45038758b3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Zencoder Online Blog',
+  domain: 'https://zencoder.online',
+  author: 'Steven Rugg',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Blog of Steven Rugg - Zencoder Online',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'stevensupergeek',
+  github: 'stevenrugg',
+  linkedin: 'stevenwrugg',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
+  defaultPageIcon: https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F811e6920-0daa-466b-9295-f642a2691939%2Fsteven.jpg?table=block&id=d429db03-e0a6-4c92-b319-ff45038758b3&spaceId=0691f2c9-bbaf-488e-afd3-890431a2f82b&width=250&userId=d5649056-205d-424f-8e9c-6e723028de51&cache=v2,
+  defaultPageCover: https://www.notion.so/images/page-cover/webb1.jpg,
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
@@ -35,7 +35,7 @@ export default siteConfig({
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: false,
+  isRedisEnabled: true,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
