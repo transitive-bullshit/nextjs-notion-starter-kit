dd9ab74dc1e89748450de3377390e95832da7513
Thy Madona <81843368+Thymadonakh@users.noreply.github.com>
2022-11-15 21:46:01 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,15 +51,15 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'b2d17b0eae6d4273a436bd324d948a26'
+    },
+//     {
+//       title: 'Contact',
+//       pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+//     }
+  ]
 })
