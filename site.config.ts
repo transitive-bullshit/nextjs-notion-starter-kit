2f9f8c275a2161bfc012324ae044aea8825b5fb8
nijama <9n0101@gmail.com>
2022-12-18 23:40:16 -0500

    Better comments

--- site.config.ts
+++ site.config.ts
@@ -1,19 +1,27 @@
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
-  // the site's root Notion page (required)
+  /* (required)
+    The site's root Notion page
+  */
   rootNotionPageId: '7875426197cf461698809def95960ebf',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
+  /* (optional)
+    if you want to restrict pages to a single notion workspace
+    (this should be a Notion ID; see the docs for how to extract this) 
+  */
   rootNotionSpaceId: null,
 
-  // basic site info (required)
+  /* (required)
+    Basic site info
+  */
   name: 'Next.js Notion Starter Kit',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Travis Fischer',
 
-  // open graph metadata (optional)
+  /* (optional)
+    Open graph metadata
+  */
   description: 'Example Next.js Notion Starter Kit Site',
 
   /* (optional)
@@ -28,32 +36,43 @@ export default siteConfig({
   // newsletter: '#', /* Optional newsletter URL */
   // youtube: '#', /* Optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX` */
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
+  /* (optional)
+    Default notion icon and cover images for site-wide consistency.
+    Page-specific values will override these site-wide defaults.
+  */
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
-  // whether or not to enable support for LQIP preview images (optional)
+  /* (optional)
+    Whether or not to enable support for LQIP preview images.
+  */
   isPreviewImageSupportEnabled: true,
 
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
+  /* (optional)
+    Whether or not redis is enabled for caching generated preview images.
+    NOTE: if you enable redis, you need to set the `REDIS_HOST` 
+    and `REDIS_PASSWORD` environment variables.
+    See the readme for more info.
+  */
   isRedisEnabled: false,
 
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
+  /* (optional)
+    Map of notion page IDs to URL paths.
+    Any pages defined here will override their default URL paths.
+    Example:
+  */
   // pageUrlOverrides: {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
   pageUrlOverrides: null,
 
-  // whether to use the default notion navigation style or a custom one with links to
-  // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
+  /* 
+    Whether to use the default notion navigation style
+    or a custom one with links to important pages.
+    To use `navigationLinks`, set `navigationStyle` to `custom`.
+  */
   navigationStyle: 'default'
   // navigationStyle: 'custom',
   // navigationLinks: [
@@ -63,7 +82,7 @@ export default siteConfig({
   //   },
   //   {
   //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //     url: 'https://twitter.com/transitive_bs'
   //   }
   // ]
 })
