a81862ab5610b26ef5deb23c57dd8dfcb10f50d4
tang <296429819@qq.com>
2023-01-05 19:17:47 +0800

    Update Footer.tsx
    use new Date().getFullYear()

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,9 +33,11 @@ export const FooterImpl: React.FC = () => {
     setHasMounted(true)
   }, [])
 
+  const currentYear  = new Date().getFullYear();
+
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2023 {config.author}</div>
+      <div className={styles.copyright}>Copyright &copy; {currentYear} {config.author}</div>
 
       <div className={styles.settings}>
         {hasMounted && (
