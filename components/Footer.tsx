1bf8791bb6aadf7acb99370ec836a6f858169dde
linwentao <linwentao@saicmobility.com>
2023-01-11 17:11:17 +0800

    footer dynamic year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export const FooterImpl: React.FC = () => {
     setHasMounted(true)
   }, [])
 
-  const year = new Date().getYear();
+  const year = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>Copyright {year} {config.author}</div>
