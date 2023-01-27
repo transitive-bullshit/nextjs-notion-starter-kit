58aba556138f17ab7a3b92e981191783d0c3dcca
robotbird <robotbird@qq.com>
2023-01-27 17:08:55 +0800

    fix:更改 github链接

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,11 +5,11 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href='https://github.com/robotbird'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
-      aria-label='View source on GitHub'
+      aria-label='robotbird GitHub'
     >
       <svg
         width='80'
