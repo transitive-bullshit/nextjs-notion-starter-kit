a1b3e82fb82b5471fd39e80e38f30c9de412513d
robotbird <robotbird@qq.com>
2023-01-28 11:44:41 +0800

    fix:默认cover 图片更改

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = withBundleAnalyzer({
       'pbs.twimg.com',
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
+      'www.jqpress.com'
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
