31da4b5ca0827dd6ca69f8b35b55ef0436dd82d6
devzhao <devzhao@126.com>
2023-02-06 01:08:10 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,14 +2,14 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '9023ef926b9f4078a9b9cff15299e560',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'devzhao的个人空间',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Travis Fischer',
 
