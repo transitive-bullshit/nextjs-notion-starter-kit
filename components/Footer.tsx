4ea5a3e23ab1291450562b9c805951463e24fc9d
Leo Thesen <leothesen@gmail.com>
2023-02-07 19:22:01 +0200

    added dynamic year 📆

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,9 +32,12 @@ export const FooterImpl: React.FC = () => {
     setHasMounted(true)
   }, [])
 
+  const d = new Date();
+	const year = d.getFullYear();
+
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>Copyright {year} {config.author}</div>
 
       <div className={styles.settings}>
         {hasMounted && (
