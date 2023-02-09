06c23bcfa07ac9b0c7c41b0a7d17d5b0515546d2
W4nde3 <45825507+W4nde3@users.noreply.github.com>
2023-02-09 17:57:42 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,27 +2,22 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '6110c35ff03548a8b94492398f42e150',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'W4nder\'s Blog',
+  domain: 'test',
+  author: 'W4nder',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -42,24 +37,29 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
+//   pageUrlOverrides: {
+//     '/javasec': 'e12dd5850aaa4e5c8aeec025842c03f2',
+//     '/toolkits': '53d483b0884a4c9291bfd763b6ec9f65',
+//     '/toys':'55625e20e0eb4158b571e3735574cdb5'
+//   }
   pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'default'
   // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationLinks: [
+    {
+      title: 'Javasec',
+      pageId: 'e12dd5850aaa4e5c8aeec025842c03f2'
+    },
+    {
+      title: 'toolkits',
+      pageId: '53d483b0884a4c9291bfd763b6ec9f65'
+    },
+    {
+      title:'toys',
+      pageId:'55625e20e0eb4158b571e3735574cdb5'
+    }
+  ]
 })
