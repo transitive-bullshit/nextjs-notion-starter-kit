dfc13d496c4e2fffe4add1cecec33c6fb603d66b
miller <scofeild806@gmail.com>
2023-02-28 10:51:10 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'fde5ac74eea345278f004482710e1af3',
+  rootNotionSpaceId: null,
 
   // basic site info (required)
   name: '搬砖艺术家',
