d5697f01a2001ee5ce72c94e019e8d6ce95d0b3a
1660i <64318388+1660i@users.noreply.github.com>
2023-03-04 07:51:24 +0700

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -55,6 +55,7 @@ export const language: string = getSiteConfig('language', 'en')
 
 // social accounts
 export const twitter: string | null = getSiteConfig('twitter', null)
+export const instagram: string | null = getSiteConfig('instagram', null)
 export const github: string | null = getSiteConfig('github', null)
 export const youtube: string | null = getSiteConfig('youtube', null)
 export const linkedin: string | null = getSiteConfig('linkedin', null)
