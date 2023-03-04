700ba0dffbc0274fc740543e8a21160e83e922f8
tmxk000 <106725757+tmxk000@users.noreply.github.com>
2023-03-04 22:52:08 +0800

    Update GitHubShareButton.tsx

--- 组件/GitHubShareButton.tsx
+++ 组件/GitHubShareButton.tsx
@@ -1,9 +1,8 @@
-//import * as React from 'react'
+import * as React from 'react'
+import styles from './styles.module.css'
 
-//import styles from './styles.module.css'
-
-//export const GitHubShareButton: React.FC = () => {
-  //return (
+export const GitHubShareButton: React.FC = () => {
+  return (
     <a
       href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
       target='_blank'
