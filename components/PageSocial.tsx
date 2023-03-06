91b3e6188696a2ec684e83e6f99b1b74bf96b17a
1660i <64318388+1660i@users.noreply.github.com>
2023-03-06 22:09:25 +0700

    Update PageSocial.tsx

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -14,6 +14,16 @@ interface SocialLink {
 }
 
 const socialLinks: SocialLink[] = [
+    config.facebook && {
+    name: 'facebook',
+    href: `https://facebook.com/${config.facebook}`,
+    title: `Facebook @${config.facebook}`,
+    icon: (
+      <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools --> <svg fill="#000000" width="800px" height="800px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M21.95 5.005l-3.306-.004c-3.206 0-5.277 2.124-5.277 5.415v2.495H10.05v4.515h3.317l-.004 9.575h4.641l.004-9.575h3.806l-.003-4.514h-3.803v-2.117c0-1.018.241-1.533 1.566-1.533l2.366-.001.01-4.256z"/>
+      </svg>
+    )
+  },
+  
   config.twitter && {
     name: 'twitter',
     href: `https://twitter.com/${config.twitter}`,
