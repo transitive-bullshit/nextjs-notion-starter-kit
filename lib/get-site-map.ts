0cacd275715c52489378b801667eac494157f77c
David Toth <tthdvd@icloud.com>
2023-03-12 15:03:51 +0100

    remove unused import

--- lib/get-site-map.ts
+++ lib/get-site-map.ts
@@ -1,4 +1,4 @@
-import { getAllPagesInSpace, uuidToId } from 'notion-utils'
+import { getAllPagesInSpace } from 'notion-utils'
 import pMemoize from 'p-memoize'
 
 import * as config from './config'
