189742969ad1c97c2b807f2d0860ead9df164b8f
guanmu <ballmanjq@gmail.com>
2023-03-16 10:26:52 +0000

    modified:   site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'AI训练师营地',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'aizhushou.site',
   author: 'ZeroZ',
 
   // open graph metadata (optional)
