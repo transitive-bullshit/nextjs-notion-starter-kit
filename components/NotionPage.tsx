0726fe07274e2786216349fa5a3ef01114a2aab4
Mas0nShi <fishilir@gmail.com>
2023-03-31 14:22:25 +0800

    fix code block not render occasionally

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -36,7 +36,9 @@ import { Waline } from './Comment'
 const Code = dynamic(() =>
   import('react-notion-x/build/third-party/code').then(async (m) => {
     // add / remove any prism syntaxes here
-    await Promise.all([
+    // TODO: temporary fix
+    // Refer: https://github.com/transitive-bullshit/nextjs-notion-starter-kit/issues/477
+    await Promise.allSettled([
       import('prismjs/components/prism-markup-templating.js'),
       import('prismjs/components/prism-markup.js'),
       import('prismjs/components/prism-bash.js'),
