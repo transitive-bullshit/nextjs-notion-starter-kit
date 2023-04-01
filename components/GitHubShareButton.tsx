8a0d55c555942d0e53f0a442b5e87d84e5634cc5
Heqing <ihuangheqing@gmail.com>
2023-04-01 08:15:57 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -1,39 +1,39 @@
-import * as React from 'react'
+// import * as React from 'react'
 
-import styles from './styles.module.css'
+// import styles from './styles.module.css'
 
-export const GitHubShareButton: React.FC = () => {
-  return (
-    <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
-      target='_blank'
-      rel='noopener noreferrer'
-      className={styles.githubCorner}
-      aria-label='View source on GitHub'
-    >
-      <svg
-        width='80'
-        height='80'
-        viewBox='0 0 250 250'
-        style={{
-          fill: '#70B7FD',
-          color: '#fff',
-          position: 'absolute',
-          zIndex: 1001,
-          top: 0,
-          right: 0,
-          border: 0,
-          transform: 'scale(1, 1)'
-        }}
-        aria-hidden='true'
-      >
-        <path d='M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z' />
-        <path
-          d='M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2'
-          fill='currentColor'
-          style={{
-            transformOrigin: '130px 106px'
-          }}
+// export const GitHubShareButton: React.FC = () => {
+//   return (
+//     <a
+//       href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+//       target='_blank'
+//       rel='noopener noreferrer'
+//       className={styles.githubCorner}
+//       aria-label='View source on GitHub'
+//     >
+//       <svg
+//         width='80'
+//         height='80'
+//         viewBox='0 0 250 250'
+//         style={{
+//           fill: '#70B7FD',
+//           color: '#fff',
+//           position: 'absolute',
+//           zIndex: 1001,
+//           top: 0,
+//           right: 0,
+//           border: 0,
+//           transform: 'scale(1, 1)'
+//         }}
+//         aria-hidden='true'
+//       >
+//         <path d='M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z' />
+//         <path
+//           d='M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2'
+//           fill='currentColor'
+//           style={{
+//             transformOrigin: '130px 106px'
+//           }}
           className={styles.octoArm}
         />
 
