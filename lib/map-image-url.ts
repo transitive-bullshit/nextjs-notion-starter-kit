997080214056110b0b542c2a30ded0bde2672ba2
Mas0nShi <fishilir@gmail.com>
2023-04-02 19:53:54 +0800

    load image directly when notion page refer third-party storage images.

--- lib/map-image-url.ts
+++ lib/map-image-url.ts
@@ -4,9 +4,22 @@ import { defaultMapImageUrl } from 'react-notion-x'
 import { defaultPageCover, defaultPageIcon } from './config'
 
 export const mapImageUrl = (url: string, block: Block) => {
+  // default to using the default page cover.
   if (url === defaultPageCover || url === defaultPageIcon) {
     return url
   }
-
+  // TODO: load image from notion host is too slow in PRC
+  // Solution (maybe): 
+  // * save cache on storage server in home country
+  // check if the url is a notion image
+  // that's means the image is referenced by the notion page
+  // in order to quickly load the image, we need to load it directly
+  if (url.startsWith("http") || url.startsWith("https")) {
+    const pu = new URL(url)
+    if (!pu.host.includes("amazonaws.com")) {
+      return url
+    }
+  }
+  // console.log("load", url, defaultMapImageUrl(url, block));
   return defaultMapImageUrl(url, block)
 }
--- next.config.js
+++ next.config.js
@@ -13,7 +13,9 @@ module.exports = withBundleAnalyzer({
       'pbs.twimg.com',
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com',
-      'transitivebullsh.it'
+      'file.notion.so', // direct link to notion file host.
+      // self hosted image storage
+      'cdn.shi1011.cn',
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
