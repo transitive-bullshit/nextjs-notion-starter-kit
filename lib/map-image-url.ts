e7eb422fc8f029ec5bc1c99c1ad29df3b843b4b3
Mas0nShi <fishilir@gmail.com>
2023-05-07 17:04:17 +0800

    rollback previous change.

--- lib/map-image-url.ts
+++ lib/map-image-url.ts
@@ -9,17 +9,18 @@ export const mapImageUrl = (url: string, block: Block) => {
     return url
   }
   // TODO: load image from notion host is too slow in PRC
+  // TODO: cancel this changes.
   // Solution (maybe): 
   // * save cache on storage server in home country
   // check if the url is a notion image
   // that's means the image is referenced by the notion page
   // in order to quickly load the image, we need to load it directly
-  if (url.startsWith("http") || url.startsWith("https")) {
-    const pu = new URL(url)
-    if (!pu.host.includes("amazonaws.com")) {
-      return url
-    }
-  }
+  // if (url.startsWith("http") || url.startsWith("https")) {
+  //   const pu = new URL(url)
+  //   if (!pu.host.includes("amazonaws.com")) {
+  //     return url
+  //   }
+  // }
   // console.log("load", url, defaultMapImageUrl(url, block));
   return defaultMapImageUrl(url, block)
 }
