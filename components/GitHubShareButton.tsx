fe81bdbb5e65e43439d393044259eb773c9e8d25
Heqing <ihuangheqing@gmail.com>
2023-05-11 14:47:41 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -1,4 +1,4 @@
-<!-- import * as React from 'react'
+import * as React from 'react'
 
 import styles from './styles.module.css'
 
@@ -46,4 +46,4 @@ export const GitHubShareButton: React.FC = () => {
     </a>
   )
 }
- -->
+
