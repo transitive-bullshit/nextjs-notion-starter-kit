4f443f58a0abde9821100d77c86aec7554de71c2
Heqing <ihuangheqing@gmail.com>
2023-05-11 14:24:56 +0800

    Update GitHubShareButton.tsx
    去掉 github icon

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+<!-- import * as React from 'react'
 
 import styles from './styles.module.css'
 
@@ -46,3 +46,4 @@ export const GitHubShareButton: React.FC = () => {
     </a>
   )
 }
+ -->
