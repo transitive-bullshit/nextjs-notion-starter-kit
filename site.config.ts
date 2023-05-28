c3b2da65b001115cf92a6cb6d7173d944dd6c23e
xipingguo456 <94697241+xipingguo456@users.noreply.github.com>
2023-05-28 12:15:24 +0800

    Update site.config.ts
    notion修改

--- site.config.ts
+++ site.config.ts
@@ -2,11 +2,11 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '6c0b664fb1244a12b50244d4231afcd5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: secret_QTVfjpr4K697wHbSSC4WMG16wYhHASV8lqePNMenG4p,
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
