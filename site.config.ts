e5a191be0ceb98cc569e045e79b861071e066f22
Jonathan Leung <me@jonl.org>
2023-06-02 19:25:53 -0400

    Update instructions for how to use navigationLinks
    I was confused by this for ~2 hours. I didn't realize that navigationStyle: 'custom' needed to be enabled to use the navigationLinks and was digging through the code wondering why navigationLinks were not showing.

---
+++ site.config.ts
@@ -0,0 +1,63 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: 'ff1a3cae900941e49cc4d4458cc2867d',
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: '684fb1cf-ad2b-40c7-8e31-c099fa61e45f',
+
+  // basic site info (required)
+  name: 'Ubiquity DAO Hub',
+  domain: 'dao.ubq.fi',
+  author: 'Ubiquity DAO',
+
+  // open graph metadata (optional)
+  description: 'Ubiquity DAO Contributors Hub',
+
+  // social usernames (optional)
+  twitter: 'UbiquityDAO',
+  github: 'ubiquity',
+  // linkedin: 'fisch2',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  // ```
+  // {
+  //   '/devpool': 'fd3d3861654f4b7b9c2702ff7842ade2',
+  // }
+  // ```
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
+  navigationStyle: 'default'
+  // navigationStyle: 'custom',
+  // navigationLinks: [
+  //   {
+  //     title: 'About',
+  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //   },
+  //   {
+  //     title: 'Contact',
+  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //   }
+  // ]
+})
