723ffbe3b02df450b464aab0c6fb6e5cb1ce1cf5
nileblack <nile.black@gmail.com>
2023-06-08 17:30:15 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -56,7 +56,7 @@ export default siteConfig({
   {
        title: 'About',
        pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  },
+  }
   //   {
   //     title: 'Contact',
   //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
