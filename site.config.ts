cd1f3a791e1e3c9add7daffa622f36cbc086836b
nileblack <nile.black@gmail.com>
2023-06-08 17:27:46 +0800

    add about

--- site.config.ts
+++ site.config.ts
@@ -52,14 +52,14 @@ export default siteConfig({
   // important pages
   navigationStyle: 'default'
   // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
+  navigationLinks: [
+  {
+       title: 'About',
+       pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  },
   //   {
   //     title: 'Contact',
   //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
   //   }
-  // ]
+  ]
 })
