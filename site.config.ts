c36bfb1a820568d6625a330e04c9d23d5cc39c23
nileblack <nile.black@gmail.com>
2023-06-08 17:37:19 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,7 +50,7 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
+  navigationStyle: 'default',
   // navigationStyle: 'custom',
   navigationLinks: [
   {
