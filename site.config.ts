f1ee7e137e20f21bb152c7fb1ef3ef9c4ecb154d
Summer X <101614185+369386973@users.noreply.github.com>
2023-06-18 11:45:38 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e4037ffe4c4c4498b5117478e655f559',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'SWIS',
+  domain: 'xiajt.site',
+  author: '夏靖涛',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'summer',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
