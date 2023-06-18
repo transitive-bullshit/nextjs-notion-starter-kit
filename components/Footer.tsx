c5fdde59fba4d8e70cd002b4ef090480fd9b74d4
Summer X <101614185+369386973@users.noreply.github.com>
2023-06-18 17:27:03 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 import { FaEnvelopeOpenText } from '@react-icons/all-files/fa/FaEnvelopeOpenText'
 import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
-import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
+import { SiVercel } from '@react-icons/all-files/si/SiVercel'
 import { FaMastodon } from '@react-icons/all-files/fa/FaMastodon'
 import { SiNotion } from '@react-icons/all-files/si/SiNotion'
 import { FaYoutube } from '@react-icons/all-files/fa/FaYoutube'
@@ -108,7 +108,7 @@ export const FooterImpl: React.FC = () => {
             target='_blank'
             rel='noopener noreferrer'
           >
-            <FaLinkedin />
+            <SiVercel />
           </a>
         )}
 
