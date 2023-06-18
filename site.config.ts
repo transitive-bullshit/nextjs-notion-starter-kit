4efd99c59f521de5931183e6f6e3396be268e1be
Summer X <101614185+369386973@users.noreply.github.com>
2023-06-18 17:05:01 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,7 +50,7 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
+  navigationStyle: 'custom'
   // navigationStyle: 'custom',
   // navigationLinks: [
   //   {
