417424beeafeee14e03adb341a6a5c029432b106
nileblack <nile.black@gmail.com>
2023-06-20 11:20:09 +0800

    Update site.config.ts
    remove github

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
-  github: 'nileblack',
+  // github: 'nileblack',
   // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
