d7f0ef585a37f667a6d2849ce548d37b9d4b5b31
Eshwar <108211386+ewshu@users.noreply.github.com>
2023-07-02 20:20:44 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Eshwar-Portfolio',
-  domain: 'https://eshu.me/',
+  domain: 'https://eshwarp.com/',
   author: 'Eshwar Pamula',
 
   // open graph metadata (optional)
