fc0de729c3c857479214547fd14ae59963b690a4
HushanCode <122870379+HushanCode@users.noreply.github.com>
2023-07-16 13:09:22 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '6756ff3cf1204075bf137c2e327e603c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://www.notion.so/zhuling/zhuling-s-blog-6756ff3cf1204075bf137c2e327e603c',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
