75a95d32aeb6329694b8ad31e49dda2d683ef13e
shanggangli <1131270314@qq.com>
2023-07-22 15:08:12 +0800

    1

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
+      href=''
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
