22b80fde51838afa817052e9263e1fe482b0af40
Dodo <dodo@acon3d.com>
2023-09-08 15:47:42 +0900

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -190,7 +190,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
   const isBlogPost =
     block?.type === 'page' && block?.parent_table === 'collection'
 
-  const showTableOfContents = !!isBlogPost
+  const showTableOfContents = true
   const minTableOfContentsItems = 3
 
   const pageAside = React.useMemo(
