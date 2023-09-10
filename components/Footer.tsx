8a096ac8a04aad6b7fa353605f0379a3813b6640
luhangsnn <luhangsun@gmail.com>
2023-09-09 23:15:17 -0700

    update copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2023 {config.author}</div>
+      <div className={styles.copyright}>Copyright 2023 {config.author} | Thanks to Notion 💛 and <a href = "https://github.com/transitive-bullshit/nextjs-notion-starter-kit">this repo</a></div>
 
       <div className={styles.settings}>
         {hasMounted && (
