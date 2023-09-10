35936f11346b18f837914199762f55d56307a11a
luhangsnn <luhangsun@gmail.com>
2023-09-10 00:06:14 -0700

    fix Notionpage and add gitignore

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -276,7 +276,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
         defaultPageCoverPosition={config.defaultPageCoverPosition}
         mapPageUrl={siteMapPageUrl}
         mapImageUrl={mapImageUrl}
-        searchNotion={config.isSearchEnabled ? searchNotion : null}
+        // searchNotion={config.isSearchEnabled ? searchNotion : null}
         pageAside={pageAside}
         footer={footer}
       />
