8182d5920781c0eb08357f251a6ee1a17cfd68db
Dodo <dodo@acon3d.com>
2023-09-13 11:13:34 +0900

    Change notion page

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'ae6c0a608fd749b4a14b1cf98f058ff7',
+  rootNotionPageId: 'f6ef8dd07b18491088435746d02aa67b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
