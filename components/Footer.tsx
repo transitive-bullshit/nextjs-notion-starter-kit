f1dcc449650365ecdf81a813ea48a36800b26e4e
Heqing <ihuangheqing@gmail.com>
2023-09-18 11:30:42 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,7 @@ export const FooterImpl: React.FC = () => {
       </div>
 
     
-{/*       <div className={styles.settings}>
+      <div className={styles.settings}>
         {hasMounted && (
           <a
             className={styles.toggleDarkMode}
@@ -52,7 +52,7 @@ export const FooterImpl: React.FC = () => {
             {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
           </a>
         )}
-      </div> */}
+      </div>
 
       <div className={styles.social}>
         {config.twitter && (
