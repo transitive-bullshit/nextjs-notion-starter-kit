71079bc0a752f53b017877b6df99582d6d50114f
Dohyeon Park <dodo@acon3d.com>
2023-09-19 14:05:21 +0900

    Install Hotjar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,22 @@ export default class MyDocument extends Document {
             />
 
             <link rel='manifest' href='/manifest.json' />
+
+            {/* Hotjar */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:3659630,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `
+              }}
+            ></script>
           </Head>
 
           <body>
