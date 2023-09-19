8f7846fbb215a9a241d82a712ccb9a34c4f0887d
Dohyeon Park <dodo@acon3d.com>
2023-09-19 13:44:11 +0900

    Change notion site

--- site.config.ts
+++ site.config.ts
@@ -2,8 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'f6ef8dd07b18491088435746d02aa67b',
-
+  rootNotionPageId: 'ae6c0a608fd749b4a14b1cf98f058ff7',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
@@ -64,4 +63,5 @@ export default siteConfig({
   //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
   //   }
   // ]
+  // includeNotionIdInUrls: true
 })
