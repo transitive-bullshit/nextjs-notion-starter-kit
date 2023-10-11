12b733b918723dbd41ec67f4227e27665edd91ba
jiangmingjiang <jiangmingjiang@meituan.com>
2023-10-11 14:57:10 +0800

    feat: remove github corner

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import styles from './styles.module.css'
+// import styles from './styles.module.css'
 
 export const GitHubShareButton: React.FC = () => {
   return (
