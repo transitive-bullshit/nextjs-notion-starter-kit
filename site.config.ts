5ebfa4c740a8c6eb9c0bd8869b6e31395092806c
jiangmingjiang <jiangmingjiang@meituan.com>
2023-10-13 14:27:19 +0800

    feat: svg favicon

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: "https://www.floatflow.com/favicon-svg.svg",
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
