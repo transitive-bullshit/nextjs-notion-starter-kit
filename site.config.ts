b5fcf340e60efe12ad6a1fdd65bc9fa7b4e5404d
Fox <877526487@qq.com>
2023-10-14 22:06:42 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Insitut de Formacio Fox',
-  domain: 'www.ifox.es',
+  domain: 'ifox.es',
   author: 'Fox',
 
   // open graph metadata (optional)
