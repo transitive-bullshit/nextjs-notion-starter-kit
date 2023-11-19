6bcbcd20bcb0e28b04149e74bce8ca98419a39da
juliamaheoi <juliaramosguedes@gmail.com>
2023-11-19 20:08:13 -0300

    setup: bootstrap

--- lib/config.ts
+++ lib/config.ts
@@ -51,7 +51,7 @@ export const name: string = getSiteConfig('name')
 export const author: string = getSiteConfig('author')
 export const domain: string = getSiteConfig('domain')
 export const description: string = getSiteConfig('description', 'Notion Blog')
-export const language: string = getSiteConfig('language', 'en')
+export const language: string = getSiteConfig('language', 'pt-BR')
 
 // social accounts
 export const twitter: string | null = getSiteConfig('twitter', null)
