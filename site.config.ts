9f408cd87661efe3d977b6d78daa4b2a2586a590
MOOD MNKY <simeon.bowman@moodmnky.com>
2024-01-04 12:54:03 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'b37a275a219d4b899d6fa650a2bd46de',
+  rootNotionPageId: 'd8a42082a8774fcba91523af7b63f826',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
