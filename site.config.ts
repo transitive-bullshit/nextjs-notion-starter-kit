8085d278f64e5304ff7fdcd458938037980dba84
wangyihao <yihao.w@nyu.edu>
2024-01-11 14:39:01 -0500

    add bilibili

--- site.config.ts
+++ site.config.ts
@@ -20,6 +20,7 @@ export default siteConfig({
   twitter: 'transitive_bs',
   github: 'transitive-bullshit',
   linkedin: 'fisch2',
+  // bilibili: '#', // optional bilibili uid
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
