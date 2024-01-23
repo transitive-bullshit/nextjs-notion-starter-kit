d54dc38dc0913713ef9e6ef6b15275224e690aad
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2024-01-23 17:14:43 +0800

    Update giscus.tsx

--- pages/giscus.tsx
+++ pages/giscus.tsx
@@ -17,7 +17,10 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   res.setHeader('Content-Type', 'text/html')
   res.write(`<div class="giscus" style="margin: 1rem; width: calc(100% - 2rem);"></div>
 <script>
-  window.self.parent.document.getElementsByClassName('notion-page-content-inner')[0].append('<script src="https://giscus.app/client.js" data-repo="lemonorangeapple/lemonorangeapple.github.io" data-repo-id="R_kgDOKvAL3w" data-category="Announcements" data-category-id="DIC_kwDOKvAL384Ccnt0" data-mapping="pathname" data-strict="0"data-reactions-enabled="0" data-emit-metadata="0" data-input-position="top" data-theme="preferred_color_scheme" data-lang="zh-CN" data-loading="lazy" crossorigin="anonymous" async></scr' + 'ipt>');
+  var tmp = window.self.parent.document.createElement('div');
+  tmp.className = "_giscus";
+  window.self.parent.document.getElementsByClassName('notion-page-content-inner')[0].appendChild(tmp);
+  window.self.parent.document.getElementsByClassName('_giscus')[0].innerHTML = ('<script src="https://giscus.app/client.js" data-repo="lemonorangeapple/lemonorangeapple.github.io" data-repo-id="R_kgDOKvAL3w" data-category="Announcements" data-category-id="DIC_kwDOKvAL384Ccnt0" data-mapping="pathname" data-strict="0"data-reactions-enabled="0" data-emit-metadata="0" data-input-position="top" data-theme="preferred_color_scheme" data-lang="zh-CN" data-loading="lazy" crossorigin="anonymous" async></scr' + 'ipt>');
 </script>`)
 
   res.end()
