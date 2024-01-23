286f67b1992d84d85c3fed2c723d42254f466f50
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2024-01-23 17:07:01 +0800

    Update giscus.tsx

--- pages/giscus.tsx
+++ pages/giscus.tsx
@@ -14,7 +14,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 
   // cache for up to one day
   res.setHeader('Cache-Control', 'public, max-age=86400, immutable')
-  res.setHeader('Content-Type', 'text/plain')
+  res.setHeader('Content-Type', 'text/html')
   res.write(`<div class="giscus" style="margin: 1rem; width: calc(100% - 2rem);"></div>
 <script>
   window.self.parent.append('<script src="https://giscus.app/client.js" data-repo="lemonorangeapple/lemonorangeapple.github.io" data-repo-id="R_kgDOKvAL3w" data-category="Announcements" data-category-id="DIC_kwDOKvAL384Ccnt0" data-mapping="pathname" data-strict="0"data-reactions-enabled="0" data-emit-metadata="0" data-input-position="top" data-theme="preferred_color_scheme" data-lang="zh-CN" data-loading="lazy" crossorigin="anonymous" async></scr' + 'ipt>');
