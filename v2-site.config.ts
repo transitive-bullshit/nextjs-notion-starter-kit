1f1dce5ca8171b70206d7e3bea65df0a4c7b754b
Dohyeon Park <dodo@acon3d.com>
2024-01-30 19:00:39 +0900

    Create config for v2

---
+++ v2-site.config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  rootNotionPageId: '4ad6d9a48f9041aeaae8888861f6af90',
+  name: '에이블러 2.0 유저 가이드',
+  domain: 'v2-guide.abler.world',
+  description: '에이블러 2.0 유저 가이드',
+  pageUrlOverrides: {}
+}
+
+// console.log(JSON.stringify(config))
