39e332106f2fd19ee8dc021ebae6de9a8cf2ffa3
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2024-02-04 20:47:57 +0800

    Update giscus.tsx

--- pages/giscus.tsx
+++ pages/giscus.tsx
@@ -21,7 +21,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   window.self.parent.document.getElementsByClassName('notion-page-content-inner')[0].appendChild(tmp);
   var range = window.self.parent.document.createRange();
   range.selectNode(window.self.parent.document.getElementsByClassName('_giscus')[0]);
-  var documentFragment = range.createContextualFragment('<div class="giscus" style="width: 100%;"></div><script src="https://giscus.app/client.js" data-repo="lemonorangeapple/lemonorangeapple.github.io" data-repo-id="R_kgDOKvAL3w" data-category="Announcements" data-category-id="DIC_kwDOKvAL384Ccnt0" data-mapping="pathname" data-strict="0"data-reactions-enabled="0" data-emit-metadata="0" data-input-position="top" data-theme="preferred_color_scheme" data-lang="zh-CN" data-loading="lazy" crossorigin="anonymous" async></scr' + 'ipt><script color="0,0,0" opacity="1" count="75" zindex="-2" src="https://blog-static.cnblogs.com/files/xiaokang01/js.js"></scr' + 'ipt>');
+  var documentFragment = range.createContextualFragment('<div class="giscus" style="width: 100%;"></div><script src="https://giscus.app/client.js" data-repo="lemonorangeapple/lemonorangeapple.github.io" data-repo-id="R_kgDOKvAL3w" data-category="Announcements" data-category-id="DIC_kwDOKvAL384Ccnt0" data-mapping="pathname" data-strict="0"data-reactions-enabled="0" data-emit-metadata="0" data-input-position="top" data-theme="preferred_color_scheme" data-lang="zh-CN" data-loading="lazy" crossorigin="anonymous" async></scr' + 'ipt>');
   window.self.parent.document.getElementsByClassName('_giscus')[0].appendChild(documentFragment);
   window.self.parent.document.querySelector("figure").style.display = 'none';
 </script>`)
