638d3c874357c6f8040d8ef5170807bffc6012b7
Onni Hakala <onni@keksi.io>
2024-02-06 14:34:45 +0100

    Remove duplicate twitter meta properties which can fallback to opengraph properties

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -56,14 +56,12 @@ export const PageHead: React.FC<
         <>
           <meta name='description' content={description} />
           <meta property='og:description' content={description} />
-          <meta name='twitter:description' content={description} />
         </>
       )}
 
       {socialImageUrl ? (
         <>
           <meta name='twitter:card' content='summary_large_image' />
-          <meta name='twitter:image' content={socialImageUrl} />
           <meta property='og:image' content={socialImageUrl} />
         </>
       ) : (
@@ -74,7 +72,6 @@ export const PageHead: React.FC<
         <>
           <link rel='canonical' href={url} />
           <meta property='og:url' content={url} />
-          <meta property='twitter:url' content={url} />
         </>
       )}
 
@@ -86,7 +83,6 @@ export const PageHead: React.FC<
       />
 
       <meta property='og:title' content={title} />
-      <meta name='twitter:title' content={title} />
       <title>{title}</title>
     </Head>
   )
