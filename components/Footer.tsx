6889a997f307b74270b6e788fb6fc2132a4124d3
buhe <bugu1986@126.com>
2024-03-07 14:51:49 +0800

    "Refactor Footer component by commenting out unused code."

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,9 +36,9 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright {currentYear} {config.author}</div>
+      {/* <div className={styles.copyright}>Copyright {currentYear} {config.author}</div> */}
 
-      <div className={styles.settings}>
+      {/* <div className={styles.settings}>
         {hasMounted && (
           <a
             className={styles.toggleDarkMode}
@@ -50,9 +50,9 @@ export const FooterImpl: React.FC = () => {
             {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
           </a>
         )}
-      </div>
+      </div> */}
 
-      <div className={styles.social}>
+      {/* <div className={styles.social}>
         {config.twitter && (
           <a
             className={styles.twitter}
@@ -135,7 +135,7 @@ export const FooterImpl: React.FC = () => {
             <FaYoutube />
           </a>
         )}
-      </div>
+      </div> */}
     </footer>
   )
 }
