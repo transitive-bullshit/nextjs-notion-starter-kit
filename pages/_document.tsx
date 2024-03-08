31eab28e9cef6b3bb8933639055cf321f88d9010
buhe <bugu1986@126.com>
2024-03-08 20:22:59 +0800

    "Refactor localStorage.setItem() in _document.tsx to always store false for the storageKey."

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -58,11 +58,11 @@ export default class MyDocument extends Document {
   } else if (supportsColorSchemeQuery) {
     // source of truth from system
     setClassOnDocumentBody(mql.matches)
-    localStorage.setItem(storageKey, mql.matches)
+    localStorage.setItem(storageKey, JSON.stringify(false))
   } else {
     // source of truth from document.body
     var isDarkMode = document.body.classList.contains(classNameDark)
-    localStorage.setItem(storageKey, JSON.stringify(isDarkMode))
+    localStorage.setItem(storageKey, JSON.stringify(false))
   }
 })();
 `
