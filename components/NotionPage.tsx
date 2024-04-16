c0d4e5ad81977d27861415956b56b7556d7ef633
buhe <bugu1986@126.com>
2024-04-16 17:15:33 +0800

    disable next/image

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -153,7 +153,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
 
   const components = React.useMemo(
     () => ({
-      nextImage: Image,
       nextLink: Link,
       Code,
       Collection,
