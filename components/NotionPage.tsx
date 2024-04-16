1e7c7c4197497e61f1b2babe9188f63513099f74
buhe <bugu1986@126.com>
2024-04-16 17:17:42 +0800

    fix lint

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import dynamic from 'next/dynamic'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
