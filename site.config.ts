670d21e1c28aca42cba8d6b822c6207448bc5e57
WhaleTiku <kujikuji@yahoo.cpm>
2024-04-26 20:34:56 +0900

    菜单个性化/添加HOMT ABOUT BLOG

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,20 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+     {
+       title: 'HOME',
+       pageId: '858447c91db0434282b31b8d08d8df83'
+     },
+     {
+      title: 'ABOUT',
+      pageId: 'About-bd3e799e3ec14c28848511e4b5450d2a'
+    },
+     {
+       title: 'BLOG',
+       pageId: '6ff6114a871849b785453bc706574282'
+     }
+   ]
 })
