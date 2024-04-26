500baf95eb8cff52d3ec932ede05f8bf127aa88d
WhaleTiku <kujikuji@yahoo.cpm>
2024-04-27 02:05:59 +0900

    Blog链接8

--- site.config.ts
+++ site.config.ts
@@ -60,13 +60,7 @@ export default siteConfig({
     },
      {
        title: 'BLOG',
-       pageId: 'https://whale-tiku.notion.site/6ff6114a871849b785453bc706574282?v=c3696e99a7074b45a1a39ea0afdc32d5&pvs=4'
+       pageId: 'Blog-1d5ee83bb5cc4956b1544d578e92beb6'
      }
    ]
 })
-// 6ff6114a871849b785453bc706574282?v=c3696e99a7074b45a1a39ea0afdc32d5
-// 6ff6114a871849b785453bc706574282
-// c3696e99a7074b45a1a39ea0afdc32d
-// 6ff6114a871849b785453bc706574282?v=c3696e99a7074b45a1a39ea0afdc32d5&pvs=4
-// 3696e99a7074b45a1a39ea0afdc32d5
-// https://whale-tiku.notion.site/6ff6114a871849b785453bc706574282?v=c3696e99a7074b45a1a39ea0afdc32d5&pvs=4
