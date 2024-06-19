7af7f8667c76bf84a80df2d95a1adba5987b2240
Hareesh Kumar <hareesh@live.in>
2024-06-20 00:13:27 +0530

    added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,7 +20,6 @@ import styles from './styles.module.css'
 export const FooterImpl: React.FC = () => {
   const [hasMounted, setHasMounted] = React.useState(false)
   const { isDarkMode, toggleDarkMode } = useDarkMode()
-  const currentYear = new Date().getFullYear()
 
   const onToggleDarkMode = React.useCallback(
     (e) => {
@@ -36,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright {currentYear} {config.author}</div>
+      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
 
       <div className={styles.settings}>
         {hasMounted && (
--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -26,13 +26,7 @@ export const PageHead: React.FC<
       <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
       <meta
         name='viewport'
-        content='width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover'
-      />
-
-      <meta name='apple-mobile-web-app-capable' content='yes' />
-      <meta
-        name='apple-mobile-web-app-status-bar-style'
-        content='black'
+        content='width=device-width, initial-scale=1, shrink-to-fit=no'
       />
 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fefffe" key="theme-color-light"/>
