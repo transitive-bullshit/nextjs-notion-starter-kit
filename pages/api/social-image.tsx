9e3e1071259f5dd27d449d0f2d253831e51c1a25
Gaurav Manek <gaurav@gauravmanek.com>
2024-07-25 22:39:40 +0800

    Bumped the deps successfully, now we are fixing up next/images.

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -15,7 +15,7 @@ const interBoldFontP = fetch(
 ).then((res) => res.arrayBuffer())
 
 export const config = {
-  runtime: 'experimental-edge'
+  runtime: 'edge'
 }
 
 export default async function OGImage(req: NextRequest) {
