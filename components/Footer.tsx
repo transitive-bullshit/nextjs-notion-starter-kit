2f51ec8c135cea92da1dd0cafda2fb70d62cecad
Prajwal Rangegowda <147972135+PrajwalRangegowda@users.noreply.github.com>
2024-09-06 08:39:06 +0530

    Update Footer.tsx linkedin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -106,7 +106,7 @@ export const FooterImpl: React.FC = () => {
         {config.linkedin && (
           <a
             className={styles.linkedin}
-            href={`https://www.linkedin.com/in/${config.linkedin}`}
+            href={`https://www.linkedin.com/company/${config.linkedin}`}
             title={`LinkedIn ${config.author}`}
             target='_blank'
             rel='noopener noreferrer'
