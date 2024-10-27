3d1150ac3b432ec712df11489cecdae61962c09d
Tai Bui Ngoc <tai.bui@employmenthero.com>
2024-10-27 11:21:15 +0700

    feature: update site info, notion id

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '12b6c2c4b7e080258259daf7f8790183',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'TaiBui Experiences',
+  domain: 'software-engineer.taibui.it',
+  author: 'Tai Bui',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
