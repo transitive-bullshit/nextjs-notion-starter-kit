c9e5ee7a8ad77b0aff2c4f66d5d74196c52728f4
Sheeraz Gul <rahuja.sheeraz@gmail.com>
2024-10-31 12:36:50 +0500

    chores: Replaced notion fav icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,12 +10,12 @@ export default class MyDocument extends Document {
         <Html lang='en'>
           <Head>
             <link rel='shortcut icon' href='/favicon.ico' />
-            <link
+            {/* <link
               rel='icon'
               type='image/png'
               sizes='32x32'
               href='favicon.png'
-            />
+            /> */}
 
             <link rel='manifest' href='/manifest.json' />
           </Head>
