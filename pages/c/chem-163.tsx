87e3c6036b013104d4f9f64555dff30820ca8df4
Divide-By-0 <aayushg@mit.edu>
2024-11-26 09:21:09 -0500

    fix url to adam cohens class

---
+++ pages/c/chem-163.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+export default function Chem163() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/frontiers-in-biophysicschem163')
+    window.location.href = '/frontiers-in-biophysicschem163'
+  }, [])
+
+  return null
+}
