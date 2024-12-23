f5d45303f5118fae6bacf55fd66679d1a20294e0
caste77an <hancastellan@naver.com>
2024-12-23 16:35:04 +0900

    modify name, domain in site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'castellan blog',
+  domain: 'castellan.vercel.com',
+  author: 'castellan',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'castellan and star story posting',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
