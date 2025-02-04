dc184003f851bfa0cc7f1c1507d3af3abb7bb5f6
Fox <49583491+FoxSpain@users.noreply.github.com>
2025-02-04 19:59:32 +0800

    Update NotionPageHeader.tsx
    恢复原源码

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -6,19 +6,10 @@ import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
 import cs from 'classnames'
 import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
 
-import { isSearchEnabled, navigationStyle } from '@/lib/config'
+import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import styles from './styles.module.css'
-
-// 这里修改了navigationLinks，添加了Bilibili链接
-const navigationLinks = [
-  {
-    title: 'Bilibili', // 超链接的文字
-    url: 'https://space.bilibili.com/18335369?spm_id_from=333.1007.0.0', // 指向Bilibili的链接
-  },
-  // 其他链接可以继续添加
-]
 
 const ToggleThemeButton = () => {
   const [hasMounted, setHasMounted] = React.useState(false)
