a621719d4e7279d13d3d854932bee073926914bd
Ahsan <idr.ahsan@gmail.com>
2025-02-13 14:38:21 +0700

    first deployment

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '14320fbba15480c28ff6dda2cad000a2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Ahsfalod',
+  domain: '',
+  author: 'Ahsan Jularso W',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Ahsan personal page',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'https://x.com/ahsfalod',
+  linkedin: 'https://www.linkedin.com/in/ahsan-jularso/',
+  Instagram: 'https://www.instagram.com/ahsfalod/#',
+  Medium: 'https://medium.com/@ahsfalod',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
