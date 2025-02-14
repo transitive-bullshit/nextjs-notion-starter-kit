0d39070a65a086a4bef7576b33384455c6236427
Fox <49583491+FoxSpain@users.noreply.github.com>
2025-02-14 21:43:19 +0800

    Update GitHubShareButton.tsx
    更改右上角超链接至github主页

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/FoxSpain/nextjs-notion-starter-kit'
+      href='https://github.com/FoxSpain'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
