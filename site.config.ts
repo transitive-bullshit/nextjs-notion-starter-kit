cfb0a91a1e300de81635dea37e72e72b08e504b4
cheungzt <122290450+cheungzt@users.noreply.github.com>
2025-03-15 11:50:23 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,15 +51,15 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+   navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: 'About',
+       pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+     },
+     {
+       title: 'Contact',
+       pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+     }
+   ]
 })
