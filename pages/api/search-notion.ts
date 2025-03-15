3153e8262f2593699822e67c5fb96391d63b7e8d
June <107529938+lemonorangeapple@users.noreply.github.com>
2025-03-15 12:02:37 +0800

    Update search-notion.ts

--- pages/api/search-notion.ts
+++ pages/api/search-notion.ts
@@ -4,9 +4,6 @@ import * as types from '../../lib/types'
 import { search } from '../../lib/notion'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') {
-    return res.status(405).send({ error: 'method not allowed' })
-  }
 
   const searchParams: types.SearchParams = req.body
 
