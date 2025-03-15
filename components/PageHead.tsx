de2b5618cfda4ec29e96e82dc18e14d92e9ce4ec
June <107529938+lemonorangeapple@users.noreply.github.com>
2025-03-15 11:46:34 +0800

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -3,13 +3,11 @@ import Head from 'next/head'
 
 import * as config from '@/lib/config'
 import * as types from '@/lib/types'
-import { getSocialImageUrl } from '@/lib/get-social-image-url'
 
 export const PageHead: React.FC<
   types.PageProps & {
     title?: string
     description?: string
-    image?: string
     url?: string
   }
 > = ({ site, title, description, pageId, image, url }) => {
