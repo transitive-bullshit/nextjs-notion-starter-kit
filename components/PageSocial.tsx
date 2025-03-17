758b9eba30dd5a0d125d85bc6c8b80369cd64b17
cheungzt <122290450+cheungzt@users.noreply.github.com>
2025-03-17 20:51:44 +0800

    Update PageSocial.tsx

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -13,16 +13,6 @@ interface SocialLink {
 }
 
 const socialLinks: SocialLink[] = [
-  config.instagram && {
-    name: 'instagram',
-    href: `https://instagram.com/${config.instagram}`,
-    title: `instagram @${config.instagram}`,
-  icon: (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-      <path d="M13 10a3 3 0 1 1-6 0q.001-.257.049-.5H6v3.997c0 .278.225.503.503.503h6.995a.503.503 0 0 0 .502-.503V9.5h-1.049q.048.243.049.5m-3 2a2 2 0 1 0-.001-4.001A2 2 0 0 0 10 12m2.4-4.1h1.199a.3.3 0 0 0 .301-.3V6.401a.3.3 0 0 0-.301-.301H12.4a.3.3 0 0 0-.301.301V7.6c.001.165.136.3.301.3M10 .4A9.6 9.6 0 0 0 .4 10a9.6 9.6 0 0 0 9.6 9.6a9.6 9.6 0 0 0 9.6-9.6A9.6 9.6 0 0 0 10 .4m5 13.489C15 14.5 14.5 15 13.889 15H6.111C5.5 15 5 14.5 5 13.889V6.111C5 5.5 5.5 5 6.111 5h7.778C14.5 5 15 5.5 15 6.111z"/>
-    </svg>
-    )
-  },
   config.xiaohongshu && {
     name: 'xiaohongshu',
     href: `https://www.xiaohongshu.com/user/profile/${config.xiaohongshu}`,
@@ -54,7 +44,18 @@ const socialLinks: SocialLink[] = [
       </svg>
     )
   },
-
+  
+  config.instagram && {
+    name: 'instagram',
+    href: `https://instagram.com/${config.instagram}`,
+    title: `instagram @${config.instagram}`,
+  icon: (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M13 10a3 3 0 1 1-6 0q.001-.257.049-.5H6v3.997c0 .278.225.503.503.503h6.995a.503.503 0 0 0 .502-.503V9.5h-1.049q.048.243.049.5m-3 2a2 2 0 1 0-.001-4.001A2 2 0 0 0 10 12m2.4-4.1h1.199a.3.3 0 0 0 .301-.3V6.401a.3.3 0 0 0-.301-.301H12.4a.3.3 0 0 0-.301.301V7.6c.001.165.136.3.301.3M10 .4A9.6 9.6 0 0 0 .4 10a9.6 9.6 0 0 0 9.6 9.6a9.6 9.6 0 0 0 9.6-9.6A9.6 9.6 0 0 0 10 .4m5 13.489C15 14.5 14.5 15 13.889 15H6.111C5.5 15 5 14.5 5 13.889V6.111C5 5.5 5.5 5 6.111 5h7.778C14.5 5 15 5.5 15 6.111z"/>
+    </svg>
+    )
+  },
+  
   config.newsletter && {
     name: 'newsletter',
     href: `${config.newsletter}`,
