701b210ec07e38a47f23f1319b42eba842b88109
cheungzt <122290450+cheungzt@users.noreply.github.com>
2025-03-17 17:11:38 +0800

    Update site-config.ts

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -17,6 +17,7 @@ export interface SiteConfig {
   youtube?: string
   zhihu?: string
   mastodon?: string
+  instagram?: string
 
   defaultPageIcon?: string | null
   defaultPageCover?: string | null
