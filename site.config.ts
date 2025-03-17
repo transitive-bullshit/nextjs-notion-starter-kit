fd3a550fdc79d58aff971f46b95558c12a5a4575
cheungzt <122290450+cheungzt@users.noreply.github.com>
2025-03-17 17:35:12 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   //twitter: 'transitive_bs',  
-  instagram: 'instagram.com/1ziat_bbbam',
+  instagram: 'https://www.instagram.com/1ziat_bbbam/',
   github: 'https://github.com/cheungzt',
   linkedin: 'linkedin.com/in/ivan-cheung-b7613b310',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
