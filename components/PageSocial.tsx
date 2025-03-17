26d8af0579bfd1799f7b80eccbc9b0b842091f31
cheungzt <122290450+cheungzt@users.noreply.github.com>
2025-03-17 19:10:47 +0800

    Update PageSocial.tsx

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -25,7 +25,7 @@ const socialLinks: SocialLink[] = [
   },
   config.xiaohongshu && {
     name: 'xiaohongshu',
-    href: `https://xiaohongshu.com/${config.xiaohongshu}`,
+    href: `https://www.xiaohongshu.com/user/profile/${config.xiaohongshu}`,
     title: `xiaohongshu @${config.xiaohongshu}`,
   icon: (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
