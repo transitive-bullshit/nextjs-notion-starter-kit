8b18d8c3483fb57eae813592aa26a28d479f50f8
cheungzt <122290450+cheungzt@users.noreply.github.com>
2025-03-17 15:23:43 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,7 +17,7 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  Rednote: 'transitive_bs',
+  twitter: 'transitive_bs',
   github: 'https://github.com/cheungzt',
   linkedin: 'linkedin.com/in/ivan-cheung-b7613b310',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
