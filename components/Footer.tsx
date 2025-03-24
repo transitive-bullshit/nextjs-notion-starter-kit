62c1a685f049452a74c4ddadf23c1480b58667bc
cheungzt <122290450+cheungzt@users.noreply.github.com>
2025-03-24 16:18:27 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,6 +36,24 @@ export function FooterImpl() {
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>
+        <div
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <a href='https://hits.seeyoufarm.com'>
+            <img
+              style={{ display: 'block', margin: '0px auto' }}
+              src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fwww.ztcheung.com&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true'
+            />
+          </a>
+          <div>Copyright 2023 {config.author} </div>
+        </div>
+      </div>
+  
+    <footer className={styles.footer}>
+      <div className={styles.copyright}>
         Copyright {currentYear} {config.author}
       </div>
 
