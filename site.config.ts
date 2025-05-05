5d16c7a33d09b3c633fcc180dd79dd935a4f46b2
TangLuIndiaPulse <tanglu1018@gmail.com>
2025-05-05 19:56:24 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,14 +2,14 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1ea0796c7a68802f9935f7395ff0b9b3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Tang's India Pulse',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Travis Fischer',
 
