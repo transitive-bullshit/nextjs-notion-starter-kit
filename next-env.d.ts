a68079edcd001b512193257dc1daff0d094307b4
Prince Kwesi <32097330+genthegreat@users.noreply.github.com>
2025-05-13 20:31:00 +0200

    Revert "Add google analytics"

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,8 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-interface Window {
-  dataLayer: any[];
-  gtag: (command: string, target: string, params?: Record<string, any>) => void;
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -61,20 +61,5 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  React.useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      if (process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && window.gtag) {
-        window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
-          page_path: url,
-        });
-      }
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return <Component {...pageProps} />
 }
