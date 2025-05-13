f10930968d319384dba4560d4c815d42a5384d00
genthegreat <ezragonyuie@gmail.com>
2025-05-13 19:36:55 +0200

    Add google analytics

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,8 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+interface Window {
+  dataLayer: any[];
+  gtag: (command: string, target: string, params?: Record<string, any>) => void;
+}
