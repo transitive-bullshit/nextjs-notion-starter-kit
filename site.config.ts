1cf5b6c89362159fb1883c784be895781c7cd8c5
rruthangel <angelavrillia04@gmail.com>
2025-05-15 21:59:34 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1f470e66092180269b8fc92efacc6081?pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'rruthangel',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'rruthangel',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
