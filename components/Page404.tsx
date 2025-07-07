72f05203b698a5e963130e2f473d394cf686301c
Jeremiah Vuong <43306105+jeremiahvuong@users.noreply.github.com>
2025-07-07 16:18:55 -0700

    404 link home routing documentation

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 
 import * as types from '@/lib/types'
 
+import { Footer } from './Footer'
 import { PageHead } from './PageHead'
 import styles from './styles.module.css'
-import { Footer } from './Footer'
 
 export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
   const title = site?.name || 'Notion Page Not Found'
@@ -16,22 +16,25 @@ export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
       <div className={styles.notFoundPageContainer}>
         <main className={styles.notFoundPage}>
           <h1>Lost?</h1>
-          <h2>We haven&apos;t made this page yet...</h2>  
-          <button onClick={() => window.location.href = '/'}>GO HOME ➚</button>
+          <h2>We haven&apos;t made this page yet...</h2>
+          {/* we can't use next/link here because it breaks dom manipulation for home page,
+          specifically the back button, so we set window.location.href instead */}
+          <button onClick={() => (window.location.href = '/')}>
+            GO HOME ➚
+          </button>
 
           {error ? (
             <p>Problem: {error.message}</p>
           ) : (
             pageId && (
               <p>
-                Make sure that Notion page &quot;{pageId}&quot; is publicly accessible.
+                Make sure that Notion page &quot;{pageId}&quot; is publicly
+                accessible.
               </p>
             )
           )}
-
-
         </main>
-        <Footer/>
+        <Footer />
       </div>
     </>
   )
