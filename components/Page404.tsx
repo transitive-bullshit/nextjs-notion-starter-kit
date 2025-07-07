757ec020ea021904f64cb655b09db1443b4e2911
Eesha <eeshaulh@gmail.com>
2025-07-07 21:58:16 +0400

    remove left padding on the footer

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -4,6 +4,7 @@ import * as types from '@/lib/types'
 
 import { PageHead } from './PageHead'
 import styles from './styles.module.css'
+import { Footer } from './Footer'
 
 export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
   const title = site?.name || 'Notion Page Not Found'
@@ -12,25 +13,25 @@ export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
     <>
       <PageHead site={site} title={title} />
 
-      <div className={styles.container}>
+      <div className={styles.notFoundPageContainer}>
         <main className={styles.notFoundPage}>
           <h1>Lost?</h1>
           <h2>We haven't made this page yet...</h2>  
-          <button onClick={() => window.location.href = '/'}>GO BACK HOME 🌱</button>
+          <button onClick={() => window.location.href = '/'}>GO HOME ➚</button>
 
           {error ? (
             <p>Problem: {error.message}</p>
           ) : (
             pageId && (
               <p>
-                Make sure that Notion page &quot;{pageId}&quot; is publicly
-                accessible.
+                Make sure that Notion page &quot;{pageId}&quot; is publicly accessible.
               </p>
             )
           )}
 
 
         </main>
+        <Footer/>
       </div>
     </>
   )
