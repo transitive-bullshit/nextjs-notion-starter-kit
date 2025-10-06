2605a2dffd5f5d0bb9a07a7e85314db8d4f9f662
jason <53788601+JeongJaeSoon@users.noreply.github.com>
2025-10-07 01:25:48 +0900

    🔧 refactor: simplify getSocialImageUrl function to always return default OGP image

--- lib/get-social-image-url.ts
+++ lib/get-social-image-url.ts
@@ -1,17 +1,6 @@
-import { api, host } from './config'
+import { host } from './config'
 
-export function getSocialImageUrl(pageId: string | undefined) {
-  try {
-    const url = new URL(api.getSocialImage, host)
-
-    if (pageId) {
-      url.searchParams.set('id', pageId)
-      return url.toString()
-    }
-  } catch (err: any) {
-    console.warn('error invalid social image url', pageId, err.message)
-  }
-
-  // Return default OGP image if no page-specific image exists
+export function getSocialImageUrl(_pageId?: string | undefined) {
+  // Always return the default OGP image
   return `${host}/ogp.png`
 }
