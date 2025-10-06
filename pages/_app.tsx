bd52e4dd43f98f26778e818e42a7ef2a963407ea
jason <53788601+JeongJaeSoon@users.noreply.github.com>
2025-10-07 00:16:17 +0900

    ✨ feat: add @vercel/analytics for enhanced site analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ import 'styles/notion.css'
 import 'styles/prism-theme.css'
 
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 import * as Fathom from 'fathom-client'
 import { useRouter } from 'next/router'
 import { posthog } from 'posthog-js'
@@ -61,5 +62,10 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
