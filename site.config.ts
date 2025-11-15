92efbbeb3c727a4c84861e188ba057e63d24c83d
openalmond <239704214+openalmond@users.noreply.github.com>
2025-11-15 16:29:21 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Open Almond Studios',
+  domain: 'www.openalmond.com',
+  author: 'Benjamin J. Schmidt',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Open Almond Studios - Rooted in Scripture, Branching into Creativity',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
