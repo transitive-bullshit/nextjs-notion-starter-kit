4ff484f48977ece8ea657244ed4046910fd7147a
plaidinum <benschmidt116@proton.me>
2025-11-24 01:21:14 -0500

    fix from main branch

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,7 +39,7 @@ export function FooterImpl() {
 
   return (
     <>
-     {/* TOP FOOTER NAV BAND */}
+      {/* TOP FOOTER NAV BAND */}
       <div className={styles.footerNav}>
         <div className={styles.footerNavInner}>
           {/* Logo block */}
@@ -104,146 +104,148 @@ export function FooterImpl() {
 
       {/* LOWER FOOTER STRIP */}
       <footer className={styles.footer}>
-        {/* COPYRIGHT */}
-        <div className={styles.copyright}>
-          © {currentYear} {config.author}. All Rights Reserved.
-        </div>
+        <div className={styles.footerInner}>
+          {/* COPYRIGHT */}
+          <div className={styles.copyright}>
+            © {currentYear} {config.author}. All Rights Reserved.
+          </div>
 
-        {/* DARK MODE TOGGLE */}
-        <div className={styles.settings}>
-          {hasMounted && (
-            <a
-              className={styles.toggleDarkMode}
-              href='#'
-              role='button'
-              onClick={onToggleDarkMode}
-              title='Toggle dark mode'
-            >
-              {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-            </a>
-          )}
-        </div>
+          {/* DARK MODE TOGGLE */}
+          <div className={styles.settings}>
+            {hasMounted && (
+              <a
+                className={styles.toggleDarkMode}
+                href='#'
+                role='button'
+                onClick={onToggleDarkMode}
+                title='Toggle dark mode'
+              >
+                {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+              </a>
+            )}
+          </div>
 
-        {/* SOCIAL ICONS */}
-        <div className={styles.social}>
-          {config.twitter && (
-            <a
-              className={styles.twitter}
-              href={`https://x.com/${config.twitter}`}
-              title={`X (Twitter) @${config.twitter}`}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <FaXTwitter />
-            </a>
-          )}
+          {/* SOCIAL ICONS */}
+          <div className={styles.social}>
+            {config.twitter && (
+              <a
+                className={styles.twitter}
+                href={`https://x.com/${config.twitter}`}
+                title={`X (Twitter) @${config.twitter}`}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaXTwitter />
+              </a>
+            )}
 
-          {config.mastodon && (
-            <a
-              className={styles.mastodon}
-              href={config.mastodon}
-              title={`Mastodon ${config.getMastodonHandle()}`}
-              rel='me'
-            >
-              <FaMastodon />
-            </a>
-          )}
+            {config.mastodon && (
+              <a
+                className={styles.mastodon}
+                href={config.mastodon}
+                title={`Mastodon ${config.getMastodonHandle()}`}
+                rel='me'
+              >
+                <FaMastodon />
+              </a>
+            )}
 
-          {config.zhihu && (
-            <a
-              className={styles.zhihu}
-              href={`https://zhihu.com/people/${config.zhihu}`}
-              title={`Zhihu @${config.zhihu}`}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <FaZhihu />
-            </a>
-          )}
+            {config.zhihu && (
+              <a
+                className={styles.zhihu}
+                href={`https://zhihu.com/people/${config.zhihu}`}
+                title={`Zhihu @${config.zhihu}`}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaZhihu />
+              </a>
+            )}
 
-          {config.github && (
-            <a
-              className={styles.github}
-              href={`https://github.com/${config.github}`}
-              title={`GitHub @${config.github}`}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <FaGithub />
-            </a>
-          )}
+            {config.github && (
+              <a
+                className={styles.github}
+                href={`https://github.com/${config.github}`}
+                title={`GitHub @${config.github}`}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaGithub />
+              </a>
+            )}
 
-          {config.discord && (
-            <a
-              className={styles.discord}
-              href={config.discord}
-              title='Discord'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <FaDiscord />
-            </a>
-          )}
+            {config.discord && (
+              <a
+                className={styles.discord}
+                href={config.discord}
+                title='Discord'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaDiscord />
+              </a>
+            )}
 
-          {config.facebook && (
-            <a
-              className={styles.facebook}
-              href={config.facebook}
-              title='Facebook'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <FaFacebook />
-            </a>
-          )}
+            {config.facebook && (
+              <a
+                className={styles.facebook}
+                href={config.facebook}
+                title='Facebook'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaFacebook />
+              </a>
+            )}
 
-          {config.instagram && (
-            <a
-              className={styles.instagram}
-              href={config.instagram}
-              title='Instagram'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <FaInstagram />
-            </a>
-          )}
+            {config.instagram && (
+              <a
+                className={styles.instagram}
+                href={config.instagram}
+                title='Instagram'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaInstagram />
+              </a>
+            )}
 
-          {config.linkedin && (
-            <a
-              className={styles.linkedin}
-              href={`https://www.linkedin.com/company/${config.linkedin}`}
-              title={`LinkedIn ${config.author}`}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <FaLinkedin />
-            </a>
-          )}
+            {config.linkedin && (
+              <a
+                className={styles.linkedin}
+                href={`https://www.linkedin.com/company/${config.linkedin}`}
+                title={`LinkedIn ${config.author}`}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaLinkedin />
+              </a>
+            )}
 
-          {config.newsletter && (
-            <a
-              className={styles.newsletter}
-              href={config.newsletter}
-              title={`Newsletter ${config.author}`}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <FaEnvelopeOpenText />
-            </a>
-          )}
+            {config.newsletter && (
+              <a
+                className={styles.newsletter}
+                href={config.newsletter}
+                title={`Newsletter ${config.author}`}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaEnvelopeOpenText />
+              </a>
+            )}
 
-          {config.youtube && (
-            <a
-              className={styles.youtube}
-              href={`https://www.youtube.com/${config.youtube}`}
-              title={`YouTube ${config.author}`}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <FaYoutube />
-            </a>
-          )}
+            {config.youtube && (
+              <a
+                className={styles.youtube}
+                href={`https://www.youtube.com/${config.youtube}`}
+                title={`YouTube ${config.author}`}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaYoutube />
+              </a>
+            )}
+          </div>
         </div>
       </footer>
     </>
