79bc25f55077cd960e239d126e7a4b0caf695583
openalmond <239704214+openalmond@users.noreply.github.com>
2025-11-24 10:24:54 -0500

    Add MailerLite script to PageHead component

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -122,6 +122,15 @@ export function PageHead({
           })}
         </script>
       )}
+      <script
+  dangerouslySetInnerHTML={{
+    __html: `(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){
+(w[f].q=w[f].q||[]).push(arguments)},l=d.createElement(e),l.async=1,l.src=u,
+n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n)
+})(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+ml('account', '1930777');`
+        }}
+      />
     </Head>
   )
 }
