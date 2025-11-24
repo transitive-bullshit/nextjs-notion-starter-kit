e12ab0193740284f8f3f3244cddf0d7492986154
openalmond <239704214+openalmond@users.noreply.github.com>
2025-11-24 10:16:20 -0500

    Fix incorrect closing tag for link element

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,7 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Alice&family=Nunito:wght@300;400;600;700&display=swap"
             rel="stylesheet"
-          />>
+          />
         </Head>
 
         <body>
