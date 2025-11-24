561b233e1b104948bf28d6c8a04471492aff64ed
openalmond <239704214+openalmond@users.noreply.github.com>
2025-11-24 09:48:07 -0500

    Integrate MailerLite Universal script in _document.tsx
    Added MailerLite Universal script for email marketing.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,15 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Alice&family=Nunito:wght@300;400;600;700&display=swap"
             rel="stylesheet"
           />
+<!-- MailerLite Universal -->
+<script>
+    (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '1930777');
+</script>
+<!-- End MailerLite Universal -->
         </Head>
 
         <body>
