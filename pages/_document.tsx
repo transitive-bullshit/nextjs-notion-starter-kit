bc4e1146584d4f5f8b137729b0417fabcf445e3a
openalmond <239704214+openalmond@users.noreply.github.com>
2025-11-24 20:34:18 -0500

    Add favicons and update manifest links
    Updated favicon and manifest links in the document.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,26 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="icon" type="image/png" sizes="32x32" href="favicon.png" />
-          <link rel="manifest" href="/manifest.json" />
-
+          {/* === FAVICONS & MANIFEST === */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/site.webmanifest" />
           {/* Google Fonts: Alice (headings) + Nunito (body) */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
