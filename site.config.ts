b05a110048c7e0f28a2e90086506f736f10514e4
WhaleKun <40340321+whalejun@users.noreply.github.com>
2026-01-29 12:02:18 +0900

    更改设置

--- site.config.ts
+++ site.config.ts
@@ -2,31 +2,29 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '3e2f077098284288ae4778c0070e652f',
+  rootNotionPageId: '858447c91db0434282b31b8d08d8df83',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'SetoLife',
+  name: 'KujiraTiku',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'KujiraTiku',
   // open graph metadata (optional)
   // open graph metadata (optional)
-  description: '赛东的日常',
+  description: '清熱鯨騰草',
 
   // social usernames (optional)
-  // myCat:'https://seto-life.vercel.app/',
-  // twitter: 'KujiraTiku',
+  myCat:'https://seto-life.vercel.app/',
+  twitter: 'KujiraTiku',
   // github: 'transitive-bullshit',
   // linkedin: 'fisch2',
-
-  mastodon: 'https://fedibird.com/@TikuTalk', // optional mastodon profile URL, provides link verification
-
+   mastodon: 'https://fedibird.com/@kujiTake', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-  // pixiv: '2781527',
+  pixiv: '2781527',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -55,8 +53,23 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
   // navigationStyle: 'default'
-
-  navigationStyle: 'default',
-  navigationLinks: []
-
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'HOME',
+      pageId: '858447c91db0434282b31b8d08d8df83'
+    },
+     {
+      title: 'ABOUT',
+      pageId: 'About-8ff57d87b0654b0b8c38b5e05947d5d7'
+    },
+     {
+       title: 'BLOG',
+       pageId: 'Blog-1d5ee83bb5cc4956b1544d578e92beb6'
+     },
+     {
+       title: 'GALLERY',
+       pageId: 'Gallery-ef6cd60929ab4ba097569da47d42a903'
+     }
+   ]
 })
