fa9359bc064f2fb7b0670610652fdc15cefe8fbd
Travis Fischer <fisch0920@gmail.com>
2026-02-14 17:16:34 -0600

    🚷

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+// import path from 'node:path'
+// import { fileURLToPath } from 'node:url'
 
-import bundleAnalyzer from '@next/bundle-analyzer'
-
-const withBundleAnalyzer = bundleAnalyzer({
-  // eslint-disable-next-line no-process-env
-  enabled: process.env.ANALYZE === 'true'
-})
-
-export default withBundleAnalyzer({
+export default {
   staticPageGenerationTimeout: 300,
   images: {
     remotePatterns: [
@@ -24,19 +17,19 @@ export default withBundleAnalyzer({
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   },
 
-  webpack: (config) => {
-    // Workaround for ensuring that `react` and `react-dom` resolve correctly
-    // when using a locally-linked version of `react-notion-x`.
-    // @see https://github.com/vercel/next.js/issues/50391
-    const dirname = path.dirname(fileURLToPath(import.meta.url))
-    config.resolve.alias.react = path.resolve(dirname, 'node_modules/react')
-    config.resolve.alias['react-dom'] = path.resolve(
-      dirname,
-      'node_modules/react-dom'
-    )
-    return config
-  },
+  // webpack: (config) => {
+  //   // Workaround for ensuring that `react` and `react-dom` resolve correctly
+  //   // when using a locally-linked version of `react-notion-x`.
+  //   // @see https://github.com/vercel/next.js/issues/50391
+  //   const dirname = path.dirname(fileURLToPath(import.meta.url))
+  //   config.resolve.alias.react = path.resolve(dirname, 'node_modules/react')
+  //   config.resolve.alias['react-dom'] = path.resolve(
+  //     dirname,
+  //     'node_modules/react-dom'
+  //   )
+  //   return config
+  // },
 
   // See https://react-tweet.vercel.app/next#troubleshooting
   transpilePackages: ['react-tweet']
-})
+}
