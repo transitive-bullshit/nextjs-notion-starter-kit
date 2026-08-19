63af1c9a732d4c9d33f54ac0f49d0f38fec04fc4
Travis Fischer <fisch0920@gmail.com>
2026-08-19 12:09:24 +0700

    🍃

--- next.config.js
+++ next.config.js
@@ -16,21 +16,5 @@ export default {
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-  },
-
-  // webpack: (config) => {
-  //   // Workaround for ensuring that `react` and `react-dom` resolve correctly
-  //   // when using a locally-linked version of `react-notion-x`.
-  //   // @see https://github.com/vercel/next.js/issues/50391
-  //   const dirname = path.dirname(fileURLToPath(import.meta.url))
-  //   config.resolve.alias.react = path.resolve(dirname, 'node_modules/react')
-  //   config.resolve.alias['react-dom'] = path.resolve(
-  //     dirname,
-  //     'node_modules/react-dom'
-  //   )
-  //   return config
-  // },
-
-  // See https://react-tweet.vercel.app/next#troubleshooting
-  transpilePackages: ['react-tweet']
+  }
 }
