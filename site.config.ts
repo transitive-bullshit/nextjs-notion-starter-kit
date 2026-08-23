d58f2a032b1559865d864bdd57bc4c79b833f7a6
Travis Fischer <fisch0920@gmail.com>
2026-08-24 01:48:24 +0700

    🤸

--- site.config.ts
+++ site.config.ts
@@ -59,10 +59,6 @@ export default siteConfig({
     {
       title: 'About',
       pageId: '8d0062776d0c4afca96eb1ace93a7538'
-    },
-    {
-      title: 'Contact',
-      pageId: '9a7ddf2973444067bbc5ce0a4e0e0058'
     }
   ]
 })
