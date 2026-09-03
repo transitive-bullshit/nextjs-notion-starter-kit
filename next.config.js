155b486eadf27bd98e7c527d19201ef22e8ca830
Travis Fischer <fisch0920@gmail.com>
2026-09-03 22:25:47 +0700

    🏔

--- next.config.js
+++ next.config.js
@@ -4,11 +4,13 @@ export default {
     remotePatterns: [
       { protocol: 'https', hostname: 'www.notion.so' },
       { protocol: 'https', hostname: 'notion.so' },
+      { protocol: 'https', hostname: 'app.notion.com' },
       { protocol: 'https', hostname: 'file.notion.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'abs.twimg.com' },
       { protocol: 'https', hostname: 'pbs.twimg.com' },
-      { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' }
+      { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' },
+      { protocol: 'https', hostname: 'img.notionusercontent.com' }
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
